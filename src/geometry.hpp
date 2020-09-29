2b5b28ff0590e436aefbcacf2f05f71985ea7cef
Keiji Maekawa <m@keijak.com>
2020-09-29 13:45:40 +0200

    int point2d

--- src/geometry.hpp
+++ src/geometry.hpp
@@ -15,6 +15,40 @@ double det(const P& p1, const P& p2) {
   return imag(p1) * real(p2) - real(p1) * imag(p2);
 }
 
+// Convex Hull by Monotone Chain
+// Populates the upper hull and the lower hull separately.
+void scan_convex_hull(const vector<P>& ps, vector<P>& upper, vector<P>& lower) {
+  for (int i = 0; i < (int)ps.size(); ++i) {
+    double ax = ps[i].real(), ay = ps[i].imag();
+    P now{ax, ay};
+    while (upper.size() >= 2) {
+      P& p2 = upper[upper.size() - 2];
+      P v1 = upper.back() - p2;
+      P v2 = now - p2;
+      if (det(v1, v2) > EPS) {
+        break;
+      }
+      upper.pop_back();
+    }
+    upper.push_back(move(now));
+  }
+  for (int i = ps.size() - 1; i >= 0; --i) {
+    double ax = ps[i].real(), ay = ps[i].imag();
+    P now{ax, ay};
+    while (lower.size() >= 2) {
+      P& p2 = lower[lower.size() - 2];
+      P v1 = lower.back() - p2;
+      P v2 = now - p2;
+      if (det(v1, v2) > EPS) {
+        break;
+      }
+      lower.pop_back();
+    }
+    lower.push_back(move(now));
+  }
+  reverse(lower.begin(), lower.end());
+}
+
 template <typename T>  // T: int, double, etc.
 struct Point2d {
   T x;
@@ -67,36 +101,3 @@ struct Point2d {
   }
 };
 // using P = Point2d<long long>;
-
-// Monotone Chain
-void scan_convex_hull(const vector<P>& ps, vector<P>& upper, vector<P>& lower) {
-  for (int i = 0; i < (int)ps.size(); ++i) {
-    double ax = ps[i].real(), ay = ps[i].imag();
-    P now{ax, ay};
-    while (upper.size() >= 2) {
-      P& p2 = upper[upper.size() - 2];
-      P v1 = upper.back() - p2;
-      P v2 = now - p2;
-      if (det(v1, v2) > EPS) {
-        break;
-      }
-      upper.pop_back();
-    }
-    upper.push_back(move(now));
-  }
-  for (int i = ps.size() - 1; i >= 0; --i) {
-    double ax = ps[i].real(), ay = ps[i].imag();
-    P now{ax, ay};
-    while (lower.size() >= 2) {
-      P& p2 = lower[lower.size() - 2];
-      P v1 = lower.back() - p2;
-      P v2 = now - p2;
-      if (det(v1, v2) > EPS) {
-        break;
-      }
-      lower.pop_back();
-    }
-    lower.push_back(move(now));
-  }
-  reverse(lower.begin(), lower.end());
-}
