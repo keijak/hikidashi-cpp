16140e9ec1720abea9a403afc8a4379373cb50a5
Keiji Maekawa <m@keijak.com>
2020-10-05 00:23:27 +0200

    floordiv and ceildiv

---
+++ src/floordiv.hpp
@@ -0,0 +1,24 @@
+
+// Returns floor(x / y).
+i64 floordiv(i64 x, i64 y) {
+  int sign = (x < 0 ? -1 : 1) * (y < 0 ? -1 : 1);
+  if (sign == 1) {
+    return abs(x) / abs(y);
+  } else {
+    i64 d = abs(y);
+    i64 cd = (abs(x) + d - 1) / d;
+    return -cd;
+  }
+}
+
+// Returns ceil(x / y).
+i64 ceildiv(i64 x, i64 y) {
+  int sign = (x < 0 ? -1 : 1) * (y < 0 ? -1 : 1);
+  if (sign == 1) {
+    i64 d = abs(y);
+    return (abs(x) + d - 1) / d;
+  } else {
+    i64 fd = abs(x) / abs(y);
+    return -fd;
+  }
+}
