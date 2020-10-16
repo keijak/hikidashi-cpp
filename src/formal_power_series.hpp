25ee02fd294cf3e8df5679ba22e57393bb2780b6
Keiji Maekawa <m@keijak.com>
2020-10-17 00:28:44 +0200

    fps

--- src/formal_power_series.hpp
+++ src/formal_power_series.hpp
@@ -6,10 +6,72 @@ struct DenseFPS {
   std::vector<T> terms;  // coefficient
 
   explicit DenseFPS(std::vector<T> terms) : terms(std::move(terms)) {}
+  DenseFPS(const DenseFPS& other) : terms(other.terms) {}
+  DenseFPS(DenseFPS&& other) : terms(std::move(other.terms)) {}
+  DenseFPS& operator=(const DenseFPS& other) { terms = other.terms; }
+  DenseFPS& operator=(DenseFPS&& other) { terms = std::move(other.terms); }
 
   int size() const { return (int)terms.size(); }
 
   const T& operator[](int i) const { return terms[i]; }
+
+  DenseFPS& operator+=(const DenseFPS& other) {
+    int n = max(size(), other.size());
+    if (n > size()) {
+      terms.resize(n);
+    }
+    int m = std::min(size(), other.size());
+    for (int i = 0; i < m; ++i) {
+      terms[i] += other[i];
+    }
+    return *this;
+  }
+
+  DenseFPS& operator-=(const DenseFPS& other) {
+    int n = max(size(), other.size());
+    if (n > size()) {
+      terms.resize(n);
+    }
+    int m = std::min(size(), other.size());
+    for (int i = 0; i < m; ++i) {
+      terms[i] -= other[i];
+    }
+    return *this;
+  }
+
+  // TODO: Use FFT.
+  DenseFPS& operator*=(const DenseFPS& other) {
+    int n = size() + other.size() - 1;
+    if (n > size()) {
+      terms.resize(n);
+    }
+    std::vector<T> res(n);
+    for (int i = 0; i < size(); ++i) {
+      for (int j = 0; j < other.size(); ++j) {
+        res[i + j] += (*this)[i] * other[j];
+      }
+    }
+    terms = std::move(res);
+    return *this;
+  }
+
+  friend DenseFPS operator+(const DenseFPS& x, const DenseFPS& y) {
+    DenseFPS res(x);
+    res += y;
+    return res;
+  }
+
+  friend DenseFPS operator-(const DenseFPS& x, const DenseFPS& y) {
+    DenseFPS res(x);
+    res -= y;
+    return res;
+  }
+
+  friend DenseFPS operator*(const DenseFPS& x, const DenseFPS& y) {
+    DenseFPS res(x);
+    res *= y;
+    return res;
+  }
 };
 
 template <typename T>
