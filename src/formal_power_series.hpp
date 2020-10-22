d07ac1866c46b07d76c60e3933141e981f6773fb
Keiji Maekawa <m@keijak.com>
2020-10-22 12:10:03 +0200

    fps

--- src/formal_power_series.hpp
+++ src/formal_power_series.hpp
@@ -10,72 +10,85 @@ using u64 = unsigned long long;
 template <typename T, int MAX_DEGREE>
 struct DenseFPS {
   // Coefficients of terms from x^0 to x^MAX_DEGREE.
-  std::vector<T> coeff;
+  std::vector<T> coeff_;
 
-  DenseFPS() : coeff(MAX_DEGREE + 1) {}  // zero-initialized
-  explicit DenseFPS(std::vector<T> c) : coeff(std::move(c)) {
-    assert((int)coeff.size() == MAX_DEGREE + 1);
+  DenseFPS() : coeff_(MAX_DEGREE + 1) {}  // zero-initialized
+  explicit DenseFPS(std::vector<T> c) : coeff_(std::move(c)) {
+    assert((int)coeff_.size() == MAX_DEGREE + 1);
   }
 
-  DenseFPS(const DenseFPS &other) : coeff(other.coeff) {}
-  DenseFPS(DenseFPS &&other) : coeff(std::move(other.coeff)) {}
+  DenseFPS(const DenseFPS &other) : coeff_(other.coeff_) {}
+  DenseFPS(DenseFPS &&other) : coeff_(std::move(other.coeff_)) {}
   DenseFPS &operator=(const DenseFPS &other) {
-    coeff = other.coeff;
+    coeff_ = other.coeff_;
     return *this;
   }
   DenseFPS &operator=(DenseFPS &&other) {
-    coeff = std::move(other.coeff);
+    coeff_ = std::move(other.coeff_);
     return *this;
   }
 
   static constexpr int size() { return MAX_DEGREE + 1; }
 
-  const T &operator[](int i) const { return coeff[i]; }
+  // Returns the coefficient of x^d.
+  const T &operator[](int d) const { return coeff_[d]; }
 
-  DenseFPS &operator+=(const DenseFPS &other) {
-    for (int i = 0; i < size(); ++i) {
-      coeff[i] += other[i];
-    }
+  DenseFPS &operator+=(const T &scalar) {
+    for (auto &x : coeff_) x += scalar;
     return *this;
+  }
+  friend DenseFPS operator+(const DenseFPS &x, const T &scalar) {
+    DenseFPS res = x;
+    res += scalar;
+    return res;
+  }
+  DenseFPS &operator+=(const DenseFPS &other) {
+    for (int i = 0; i < size(); ++i) coeff_[i] += other[i];
+    return *this;
+  }
+  friend DenseFPS operator+(const DenseFPS &x, const DenseFPS &y) {
+    DenseFPS res = x;
+    res += y;
+    return res;
   }
 
   DenseFPS &operator-=(const DenseFPS &other) {
-    for (int i = 0; i < size(); ++i) {
-      coeff[i] -= other[i];
-    }
+    for (int i = 0; i < size(); ++i) coeff_[i] -= other[i];
     return *this;
   }
+  friend DenseFPS operator-(const DenseFPS &x, const DenseFPS &y) {
+    DenseFPS res = x;
+    res -= y;
+    return res;
+  }
 
+  DenseFPS &operator*=(const T &scalar) {
+    for (auto &x : coeff_) x *= scalar;
+    return *this;
+  }
+  friend DenseFPS operator*(const DenseFPS &x, const T &scalar) {
+    DenseFPS res = x;
+    res *= scalar;
+    return res;
+  }
+  DenseFPS &operator*=(const DenseFPS &other) {
+    *this = this->mul_naive(other);
+    return *this;
+  }
+  friend DenseFPS operator*(const DenseFPS &x, const DenseFPS &y) {
+    return x.mul_naive(y);
+  }
+
+ private:
   // Naive multiplication. O(N^2)
   DenseFPS mul_naive(const DenseFPS &other) const {
     DenseFPS res;
     for (int i = 0; i < size(); ++i) {
       for (int j = 0; i + j < size(); ++j) {
-        res.coeff[i + j] += (*this)[i] * other[j];
+        res.coeff_[i + j] += (*this)[i] * other[j];
       }
     }
     return res;
-  }
-
-  DenseFPS &operator*=(const DenseFPS &other) {
-    *this = this->mul_naive(other);
-    return *this;
-  }
-
-  friend DenseFPS operator+(const DenseFPS &x, const DenseFPS &y) {
-    DenseFPS res(x);
-    res += y;
-    return res;
-  }
-
-  friend DenseFPS operator-(const DenseFPS &x, const DenseFPS &y) {
-    DenseFPS res(x);
-    res -= y;
-    return res;
-  }
-
-  friend DenseFPS operator*(const DenseFPS &x, const DenseFPS &y) {
-    return x.mul_naive(y);
   }
 };
 
@@ -85,7 +98,7 @@ DenseFPS<ModInt, MAX_DEGREE> fps_mul_ntt(
     const DenseFPS<ModInt, MAX_DEGREE> &x,
     const DenseFPS<ModInt, MAX_DEGREE> &y) {
   static_assert(ModInt::mod() != 1'000'000'007);  // Must be NTT-friendly MOD!
-  auto z = atcoder::convolution(x.coeff, y.coeff);
+  auto z = atcoder::convolution(x.coeff_, y.coeff_);
   z.resize(MAX_DEGREE + 1);  // Maybe shrink.
   return {std::move(z)};
 }
@@ -105,7 +118,7 @@ DenseFPS<ModInt, MAX_DEGREE> fps_mul_mod(
   auto zll = atcoder::convolution_ll(xll, yll);
   DenseFPS<ModInt, MAX_DEGREE> res;
   for (int i = 0; i <= MAX_DEGREE; ++i) {
-    res.coeff[i] = zll[i];
+    res.coeff_[i] = zll[i];
   }
   return res;
 }
@@ -114,7 +127,7 @@ DenseFPS<ModInt, MAX_DEGREE> fps_mul_mod(
 template <int MAX_DEGREE>
 DenseFPS<i64, MAX_DEGREE> fps_mul_ll(const DenseFPS<i64, MAX_DEGREE> &x,
                                      const DenseFPS<i64, MAX_DEGREE> &y) {
-  auto z = atcoder::convolution_ll(x.coeff, y.coeff);
+  auto z = atcoder::convolution_ll(x.coeff_, y.coeff_);
   z.resize(MAX_DEGREE + 1);  // Maybe shrink.
   return DenseFPS<i64, MAX_DEGREE>(std::move(z));
 }
@@ -123,7 +136,7 @@ template <typename T, int MAX_DEGREE, typename Func>
 DenseFPS<T, MAX_DEGREE> fps_pow_generic(const DenseFPS<T, MAX_DEGREE> &x, u64 t,
                                         Func mulfunc) {
   DenseFPS<T, MAX_DEGREE> base = x, res;
-  res.coeff[0] = 1;
+  res.coeff_[0] = 1;
   while (t) {
     if (t & 1) res = mulfunc(res, base);
     base = mulfunc(base, base);
@@ -154,33 +167,97 @@ DenseFPS<i64, MAX_DEGREE> fps_pow_ll(const DenseFPS<i64, MAX_DEGREE> &x,
 template <typename T>
 class SparseFPS {
   int size_;
-  std::vector<i64> degree;
-  std::vector<T> coeff;
+  std::vector<int> degree_;
+  std::vector<T> coeff_;
 
-  explicit SparseFPS(std::vector<std::pair<i64, T>> terms)
-      : size_(terms.size()), degree(size_), coeff(size_) {
-    // Sort by degree in ascending order.
+  SparseFPS() : size_(0) {}
+
+  explicit SparseFPS(std::vector<std::pair<int, T>> terms)
+      : size_(terms.size()), degree_(size_), coeff_(size_) {
+    // Sort by degree_ in ascending order.
     sort(terms.begin(), terms.end());
     for (int i = 0; i < size_; ++i) {
-      degree[i] = terms[i].first;
-      coeff[i] = terms[i].second;
+      degree_[i] = terms[i].first;
+      coeff_[i] = terms[i].second;
     }
   }
 
-  int size() const { return size_; }
+  inline int size() const { return size_; }
+  inline int degree(int i) const { return degree_[i]; }
+  inline const T &coeff(int i) const { return coeff_[i]; }
 
-  void add(i64 degree, T coeff) {
-    if (not degree.empty()) {
-      assert(degree > degree.back());
+  void emplace_back(int d, T c) {
+    if (not degree_.empty()) {
+      assert(d > degree_.back());
     }
-    degree.push_back(std::move(degree));
-    coeff.push_back(std::move(coeff));
+    degree_.push_back(std::move(d));
+    coeff_.push_back(std::move(c));
+    ++size_;
   }
 
-  const T &operator[](int i) const {
-    auto it = std::lower_bound(degree.begin(), degree.end(), i);
-    if (it == degree.end()) return (T)(0);
-    int j = std::distance(degree.begin(), it);
-    return coeff[j];
+  // Returns the coefficient of x^d.
+  T operator[](int d) const {
+    auto it = std::lower_bound(degree_.begin(), degree_.end(), d);
+    if (it == degree_.end() or *it != d) return (T)(0);
+    int j = std::distance(degree_.begin(), it);
+    return coeff_[j];
+  }
+
+  SparseFPS &operator+=(const T &scalar) {
+    for (auto &x : coeff_) x += scalar;
+    return *this;
+  }
+  friend SparseFPS operator+(const SparseFPS &x, const T &scalar) {
+    SparseFPS res = x;
+    res += scalar;
+    return res;
+  }
+  SparseFPS &operator+=(const SparseFPS &other) {
+    *this = this->add(other);
+    return *this;
+  }
+  friend SparseFPS operator+(const SparseFPS &x, const SparseFPS &y) {
+    return x.add(y);
+  }
+
+  SparseFPS &operator*=(const T &scalar) {
+    for (auto &x : coeff_) x *= scalar;
+    return *this;
+  }
+  friend SparseFPS operator*(const SparseFPS &x, const T &scalar) {
+    SparseFPS res = x;
+    res *= scalar;
+    return res;
+  }
+
+  SparseFPS &operator-=(const SparseFPS &other) {
+    *this = this->add(other * -1);
+    return *this;
+  }
+  friend SparseFPS operator-(const SparseFPS &x, const SparseFPS &y) {
+    return x.add(y * -1);
+  }
+
+ private:
+  SparseFPS add(const SparseFPS &other) const {
+    SparseFPS res;
+    for (int i = 0, j = 0; i < size();) {
+      const int deg = this->degree(i);
+      for (; j < other.size() and other.degree(j) < deg; ++j) {
+        res.emplace_back(other.degree(j), other.coeff(j));
+      }
+      T c = this->coeff(i);
+      if (j < other.size() and other.degree(j) == deg) {
+        c += other.coeff(j);
+        ++j;
+      }
+      if (c != 0) {
+        res.emplace_back(deg, c);
+      }
+    }
+    for (; j < other.size(); ++j) {
+      res.emplace_back(other.degree(j), other.coeff(j));
+    }
+    return res;
   }
 };
