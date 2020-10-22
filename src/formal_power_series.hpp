bb8e4b837435f6055f2ba9564f75875502cb5b65
Keiji Maekawa <m@keijak.com>
2020-10-22 10:51:07 +0200

    fps

--- src/formal_power_series.hpp
+++ src/formal_power_series.hpp
@@ -1,83 +1,69 @@
 #include <bits/stdc++.h>
 using i64 = long long;
+using u64 = unsigned long long;
 
+// Formal Power Series (dense format).
 template <typename T, int MAX_DEGREE>
 struct DenseFPS {
-  std::vector<T> coeff;  // coefficient
+  // Coefficients of terms from x^0 to x^MAX_DEGREE.
+  std::vector<T> coeff;
 
-  DenseFPS() = default;
-  explicit DenseFPS(std::vector<T> coeff) : coeff(std::move(coeff)) {}
-  explicit DenseFPS(T scalar) : DenseFPS(std::vector<T>(1, scalar)) {}
-  DenseFPS(const DenseFPS& other) : coeff(other.coeff) {}
-  DenseFPS(DenseFPS&& other) : coeff(std::move(other.coeff)) {}
-  DenseFPS& operator=(const DenseFPS& other) { coeff = other.coeff; }
-  DenseFPS& operator=(DenseFPS&& other) { coeff = std::move(other.coeff); }
+  DenseFPS() : coeff(MAX_DEGREE + 1) {}  // zero-initialized
+  explicit DenseFPS(std::vector<T> c) : coeff(std::move(c)) {}
 
-  int size() const { return (int)coeff.size(); }
+  DenseFPS(const DenseFPS &other) : coeff(other.coeff) {}
+  DenseFPS(DenseFPS &&other) : coeff(std::move(other.coeff)) {}
+  DenseFPS &operator=(const DenseFPS &other) {
+    coeff = other.coeff;
+    return *this;
+  }
+  DenseFPS &operator=(DenseFPS &&other) {
+    coeff = std::move(other.coeff);
+    return *this;
+  }
 
-  const T& operator[](int i) const { return coeff[i]; }
+  static constexpr int size() { return MAX_DEGREE + 1; }
 
-  DenseFPS& operator+=(const DenseFPS& other) {
-    int n = max(size(), other.size());
-    if (n > size()) {
-      coeff.resize(n);
-    }
-    int m = std::min(size(), other.size());
-    for (int i = 0; i < m; ++i) {
+  const T &operator[](int i) const { return coeff[i]; }
+
+  DenseFPS &operator+=(const DenseFPS &other) {
+    for (int i = 0; i < size(); ++i) {
       coeff[i] += other[i];
     }
     return *this;
   }
 
-  DenseFPS& operator-=(const DenseFPS& other) {
-    int n = max(size(), other.size());
-    if (n > size()) {
-      coeff.resize(n);
-    }
-    int m = std::min(size(), other.size());
-    for (int i = 0; i < m; ++i) {
+  DenseFPS &operator-=(const DenseFPS &other) {
+    for (int i = 0; i < size(); ++i) {
       coeff[i] -= other[i];
     }
     return *this;
   }
 
-  DenseFPS& operator*=(const DenseFPS& other) {
-    int n = std::min(int(size() + other.size() - 1), MAX_DEGREE + 1);
-    auto res = atcoder::convolution(coeff, other.coeff);
-    res.resize(n);
-    this->coeff = std::move(res);
-    return *this;
-  }
-
-  DenseFPS& mul(const DenseFPS& other) {
-    int n = std::min(int(size() + other.size() - 1), MAX_DEGREE + 1);
-    if (n > size()) {
-      terms.resize(n);
-    }
-    std::vector<T> res(n);
+  DenseFPS &operator*=(const DenseFPS &other) {
+    std::vector<T> res(size());
     for (int i = 0; i < size(); ++i) {
-      for (int j = 0; j < other.size(); ++j) {
-        if (i + j >= n) break;
+      for (int j = 0; i + j < size(); ++j) {
         res[i + j] += (*this)[i] * other[j];
       }
     }
-    terms = std::move(res);
+    coeff = std::move(res);
     return *this;
   }
 
-  friend DenseFPS operator+(const DenseFPS& x, const DenseFPS& y) {
+  friend DenseFPS operator+(const DenseFPS &x, const DenseFPS &y) {
     DenseFPS res(x);
     res += y;
     return res;
   }
 
-  friend DenseFPS operator-(const DenseFPS& x, const DenseFPS& y) {
+  friend DenseFPS operator-(const DenseFPS &x, const DenseFPS &y) {
     DenseFPS res(x);
     res -= y;
     return res;
   }
 
-  friend DenseFPS operator*(const DenseFPS& x, const DenseFPS& y) {
+  friend DenseFPS operator*(const DenseFPS &x, const DenseFPS &y) {
     DenseFPS res(x);
     res *= y;
     return res;
@@ -95,107 +81,75 @@ struct DenseFPS {
   }
 };
 
-template <typename T>
-struct DenseFPS {
-  std::vector<T> terms;  // coefficient
+// Fast polynomial multiplication by single NTT.
+template <typename ModInt, int MAX_DEGREE>
+DenseFPS<ModInt, MAX_DEGREE> mul_ntt(const DenseFPS<ModInt, MAX_DEGREE> &x,
+                                     const DenseFPS<ModInt, MAX_DEGREE> &y) {
+  static_assert(ModInt::mod() != 1'000'000'007);  // Must be NTT-friendly MOD!
+  auto z = atcoder::convolution(x.coeff, y.coeff);
+  z.resize(MAX_DEGREE + 1);  // Maybe shrink.
+  return {std::move(z)};
+}
 
-  explicit DenseFPS(std::vector<T> terms) : terms(std::move(terms)) {}
-  explicit DenseFPS(T scalar) : DenseFPS(std::vector<T>(1, scalar)) {}
-  DenseFPS(const DenseFPS& other) : terms(other.terms) {}
-  DenseFPS(DenseFPS&& other) : terms(std::move(other.terms)) {}
-  DenseFPS& operator=(const DenseFPS& other) { terms = other.terms; }
-  DenseFPS& operator=(DenseFPS&& other) { terms = std::move(other.terms); }
-
-  int size() const { return (int)terms.size(); }
-
-  const T& operator[](int i) const { return terms[i]; }
-
-  DenseFPS& operator+=(const DenseFPS& other) {
-    int n = max(size(), other.size());
-    if (n > size()) {
-      terms.resize(n);
-    }
-    int m = std::min(size(), other.size());
-    for (int i = 0; i < m; ++i) {
-      terms[i] += other[i];
-    }
-    return *this;
+// Polynomial multiplication by NTT + Garner (arbitrary mod).
+template <typename ModInt, int MAX_DEGREE>
+DenseFPS<ModInt, MAX_DEGREE> mul_mod(const DenseFPS<ModInt, MAX_DEGREE> &x,
+                                     const DenseFPS<ModInt, MAX_DEGREE> &y) {
+  std::vector<i64> xll(x.size()), yll(y.size());
+  for (int i = 0; i < x.size(); ++i) {
+    xll[i] = x[i].val();
   }
-
-  DenseFPS& operator-=(const DenseFPS& other) {
-    int n = max(size(), other.size());
-    if (n > size()) {
-      terms.resize(n);
-    }
-    int m = std::min(size(), other.size());
-    for (int i = 0; i < m; ++i) {
-      terms[i] -= other[i];
-    }
-    return *this;
+  for (int i = 0; i < y.size(); ++i) {
+    yll[i] = y[i].val();
   }
-
-  // TODO: Use FFT.
-
-  friend DenseFPS operator+(const DenseFPS& x, const DenseFPS& y) {
-    DenseFPS res(x);
-    res += y;
-    return res;
+  auto zll = atcoder::convolution_ll(xll, yll);
+  DenseFPS<ModInt, MAX_DEGREE> res;
+  for (int i = 0; i <= MAX_DEGREE; ++i) {
+    res.coeff[i] = zll[i];
   }
+  return res;
+}
 
-  friend DenseFPS operator-(const DenseFPS& x, const DenseFPS& y) {
-    DenseFPS res(x);
-    res -= y;
-    return res;
-  }
+// Polynomial multiplication by NTT + Garner (long long).
+template <int MAX_DEGREE>
+DenseFPS<i64, MAX_DEGREE> mul_ll(const DenseFPS<i64, MAX_DEGREE> &x,
+                                 const DenseFPS<i64, MAX_DEGREE> &y) {
+  auto z = atcoder::convolution_ll(x.coeff, y.coeff);
+  z.resize(MAX_DEGREE + 1);  // Maybe shrink.
+  return DenseFPS<i64, MAX_DEGREE>(std::move(z));
+}
 
-  friend DenseFPS operator*(const DenseFPS& x, const DenseFPS& y) {
-    DenseFPS res(x);
-    res *= y;
-    return res;
-  }
-
-  DenseFPS pow(unsigned long long t) const {
-    DenseFPS base = *this;
-    DenseFPS res = T(1);
-    while (t) {
-      if (t & 1) res *= base;
-      base *= base;
-      t >>= 1;
-    }
-    return res;
-  }
-};
-
+// Formal Power Series (sparse format).
 template <typename T>
 class SparseFPS {
   int size_;
-  std::vector<i64> degrees;
-  std::vector<T> coeffs;
+  std::vector<i64> degree;
+  std::vector<T> coeff;
 
   explicit SparseFPS(std::vector<std::pair<i64, T>> terms)
-      : size_(terms.size()), degrees(size_), coeffs(size_) {
+      : size_(terms.size()), degree(size_), coeff(size_) {
     // Sort by degree in ascending order.
     sort(terms.begin(), terms.end());
     for (int i = 0; i < size_; ++i) {
-      degrees[i] = terms[i].first;
-      coeffs[i] = terms[i].second;
+      degree[i] = terms[i].first;
+      coeff[i] = terms[i].second;
     }
   }
 
   int size() const { return size_; }
 
   void add(i64 degree, T coeff) {
-    if (not degrees.empty()) {
-      assert(degree > degrees.back());
+    if (not degree.empty()) {
+      assert(degree > degree.back());
     }
-    degrees.push_back(std::move(degree));
-    coeffs.push_back(std::move(coeff));
+    degree.push_back(std::move(degree));
+    coeff.push_back(std::move(coeff));
   }
 
-  const T& operator[](int i) const {
-    auto it = std::lower_bound(degrees.begin(), degrees.end(), i);
-    if (it == degrees.end()) return (T)(0);
-    int j = std::distance(degrees.begin(), it);
-    return coeffs[j];
+  const T &operator[](int i) const {
+    auto it = std::lower_bound(degree.begin(), degree.end(), i);
+    if (it == degree.end()) return (T)(0);
+    int j = std::distance(degree.begin(), it);
+    return coeff[j];
   }
 };
