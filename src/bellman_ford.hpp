da6fb9a63c40a4664a1824d97495029f711ed16d
Keiji Maekawa <m@keijak.com>
2020-10-27 13:00:14 +0100

    bellman ford negative cycle detection per node

--- src/bellman_ford.hpp
+++ src/bellman_ford.hpp
@@ -1,42 +1,48 @@
-#include <utility>
+#include <unordered_set>
 #include <vector>
 
 using i64 = long long;
-const i64 INF = 1LL << 40;
 
-// Find shortest path from a start node.
-// Allows edges with negative cost.
-pair<vector<i64>, bool> bellman_ford(
-    const int start, const vector<vector<pair<int, i64>>>& adj) {
-  const int N = static_cast<int>(adj.size());
-  vector<i64> dist(N, INF);  // distance from the start node.
-  dist[start] = 0;
+// Find the shortest path from the start node and detects negative cycle.
+// The first return value indicates the distance from the start node.
+// The second return value indicates which nodes have a negative cycle in a
+// path from the start node.
+std::pair<std::vector<i64>, std::vector<bool>> bellman_ford(
+    const std::vector<std::vector<std::pair<int, i64>>> &adj, const int start) {
+  const int n = int(adj.size());
+  const i64 INF = 1e18;
+  std::vector<i64> min_dist(n, INF);  // distance from the start node.
+  min_dist[start] = 0;
+  std::vector<bool> dist_inf(n, false);  // nodes in negative infinity distance.
 
   for (int k = 0;; ++k) {
-    bool update = false;
-    REP(i, N) {
-      if (dist[i] == INF) {
-        continue;  // We haven't reached i yet.
-      }
-      for (const pair<int, i64>& edge : adj[i]) {
-        int j = edge.first;  // from i to j
-        i64 cost = edge.second;
-        if (dist[j] > dist[i] + cost) {
-          dist[j] = dist[i] + cost;
-          update = true;
+    std::unordered_set<int> updated;
+    for (int i = 0; i < n; ++i) {
+      const auto di = min_dist[i];
+      if (di == INF) continue;  // We haven't reached i yet.
+      for (auto [j, dist] : adj[i]) {
+        if (dist_inf[j]) continue;
+        if (min_dist[j] > di + dist) {
+          min_dist[j] = di + dist;
+          updated.insert(j);
         }
       }
     }
-    if (!update) {
-      return {std::move(dist), false};
+
+    if (updated.empty()) {
+      return {std::move(min_dist), std::move(dist_inf)};
     }
-    // Negative cycle detection.
-    // When there's no negative cycle, at least one node gets the shortest
-    // distance determined for each iteration. If we have gone through N+1
-    // iterations and still have an update, there must be a negative cycle.
-    if (k == N) {
-      return {std::move(dist), true};
+
+    // Negative cycle detection
+    if (k >= n) {
+      // When there's no negative cycle, at least one node gets the shortest
+      // distance determined in each iteration. If we have gone through N+1
+      // iterations and still have an update, there must be a negative cycle.
+      for (auto u : updated) {
+        dist_inf[u] = true;
+      }
+      updated.clear();
     }
   }
-  // Never reach here.
+  assert(false);  // Never reach here.
 }
