c969d810d4b75ebea2e9c0fd3fe4ee259b77ef99
Keiji Maekawa <m@keijak.com>
2020-11-02 21:38:38 +0100

    Abstract Data on UnionFind

--- src/unionfind.hpp
+++ src/unionfind.hpp
@@ -1,34 +1,73 @@
+#include <vector>
+
 struct UnionFind {
-  vector<int> data;  // positive: parent, negative: size
+  mutable std::vector<int> par;  // positive: parent, negative: size
   int num_roots;
 
-  explicit UnionFind(size_t sz) : data(sz, -1), num_roots(sz) {}
+  explicit UnionFind(int sz) : par(sz, -1), num_roots(sz) {}
 
   bool unite(int x, int y) {
     x = find(x), y = find(y);
     if (x == y) return false;
-    if (data[x] > data[y]) swap(x, y);
-    data[x] += data[y];
-    data[y] = x;
+    if (par[x] > par[y]) std::swap(x, y);
+    par[x] += par[y];
+    par[y] = x;
     --num_roots;
     return true;
   }
 
-  int find(int k) {
-    if (data[k] < 0) return k;
-    return data[k] = find(data[k]);
+  int find(int k) const {
+    if (par[k] < 0) return k;
+    return par[k] = find(par[k]);
   }
 
-  int size(int k) { return -data[find(k)]; }
+  int size(int k) const { return -par[find(k)]; }
 
-  bool same(int x, int y) { return find(x) == find(y); }
+  bool same(int x, int y) const { return find(x) == find(y); }
 
-  vector<int> roots() {
-    vector<int> res;
+  std::vector<int> roots() const {
+    std::vector<int> res;
     res.reserve(num_roots);
-    for (int i = 0; i < (int)data.size(); ++i) {
-      if (data[i] < 0) res.push_back(i);
+    for (int i = 0; i < (int)par.size(); ++i) {
+      if (par[i] < 0) res.push_back(i);
     }
     return res;
+  }
+};
+
+template <typename Abelian>  // Abelian Monoid
+struct AggregatableUnionFind : public UnionFind {
+  using T = typename Abelian::T;
+  std::vector<T> agg_data;
+
+  explicit AggregatableUnionFind(std::vector<T> data)
+      : UnionFind(data.size()), agg_data(std::move(data)) {}
+
+  bool unite(int x, int y) const {
+    int rx = this->find(x), ry = this->find(y);
+    if (not UnionFind::unite(x, y)) return false;
+    int r = this->find(x);
+    agg_data[r] = Abelian::op(agg_data[rx], agg_data[ry]);
+    return true;
+  }
+
+  T agg(int x) const { return agg_data[this->find(x)]; }
+};
+
+template <typename Abelian>  // Abelian Group
+struct UpdatableUnionFind : public AggregatableUnionFind<Abelian> {
+  using T = typename Abelian::T;
+  std::vector<T> leaf_data;
+
+  explicit UpdatableUnionFind(std::vector<T> data)
+      : AggregatableUnionFind<Abelian>(data), leaf_data(std::move(data)) {}
+
+  void set(int x, T val) {
+    int r = this->find(x);
+    T inv_val = Abelian::invert(std::move(leaf_data[x]));
+    leaf_data[x] = std::move(val);
+    this->agg_data[r] = Abelian::op(
+        Abelian::op(std::move(this->agg_data[r]), std::move(inv_val)),
+        leaf_data[x]);
   }
 };
