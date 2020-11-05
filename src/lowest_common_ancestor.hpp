d86810d18d56aef5c2cfbc9c71e402201bb9dec8
Keiji Maekawa <m@keijak.com>
2020-11-05 10:26:59 +0100

    lca

--- src/lowest_common_ancestor.hpp
+++ src/lowest_common_ancestor.hpp
@@ -3,50 +3,52 @@ class LCA {
  public:
   const int n;  // number of nodes
   const vector<vector<int>> &adj;
-  vector<vector<int>> parent;
+  vector<vector<int>> upper;  // 2^k upper node
   vector<int> depth;
 
   explicit LCA(const Graph &g)
-      : n(g.n), adj(g.adj), parent(K, vector<int>(g.n, -1)), depth(g.n, -1) {
+      : n(g.n), adj(g.adj), upper(K, vector<int>(g.n, -1)), depth(g.n, -1) {
     dfs(0, -1, 0);
     for (int k = 0; k + 1 < K; k++) {
       REP(v, n) {
-        if (parent[k][v] < 0) {
-          parent[k + 1][v] = -1;
+        if (upper[k][v] < 0) {
+          upper[k + 1][v] = -1;
         } else {
-          parent[k + 1][v] = parent[k][parent[k][v]];
+          upper[k + 1][v] = upper[k][upper[k][v]];
         }
       }
     }
   }
 
   // Returns the node ID of the lowest common ancestor.
-  int query(int u, int v) const {
+  int ancestor(int u, int v) const {
     if (depth[u] > depth[v]) swap(u, v);
     for (int k = 0; k < K; k++) {
       if ((depth[v] - depth[u]) & (1 << k)) {
-        v = parent[k][v];
+        v = upper[k][v];
       }
     }
     if (u == v) return u;
     for (int k = K - 1; k >= 0; k--) {
-      if (parent[k][u] != parent[k][v]) {
-        u = parent[k][u];
-        v = parent[k][v];
+      if (upper[k][u] != upper[k][v]) {
+        u = upper[k][u];
+        v = upper[k][v];
       }
     }
-    return parent[0][u];
+    return upper[0][u];
   }
+
+  int parent(int v) const { return upper[0][v]; }
 
   // Returns the distance (number of edges) between two nodes.
   int distance(int u, int v) const {
-    int p = query(u, v);
+    int p = ancestor(u, v);
     return (depth[u] - depth[p]) + (depth[v] - depth[p]);
   }
 
  private:
   void dfs(int v, int p, int d) {
-    parent[0][v] = p;
+    upper[0][v] = p;
     depth[v] = d;
     for (auto x : adj[v]) {
       if (x == p) continue;
@@ -54,5 +56,5 @@ class LCA {
     }
   }
 
-  static const int K = 30;  // max parent lookup (2^K)
+  static const int K = 30;  // max upper lookup (2^K)
 };
