dbf70e67f70bb4bbe7014297e25fdb0c183a7244
Keiji Maekawa <m@keijak.com>
2020-11-11 09:00:17 +0100

    hld

---
+++ src/heavy_light_decomposition.hpp
@@ -0,0 +1,48 @@
+#include <vector>
+
+// Heavy-Light Decomposition and Euler Tour
+struct HLD {
+  int n;
+  std::vector<std::vector<int>> adj;
+  std::vector<int> sub_size, et_in, et_out, component_root;
+  int root;
+  int counter;
+
+  explicit HLD(std::vector<std::vector<int>> g, int root_ = 0)
+      : n(int(g.size())),
+        adj(g),
+        sub_size(n),
+        et_in(n),
+        et_out(n),
+        component_root(n),
+        root(root_),
+        counter(0) {
+    dfs_sz(root, -1);
+
+    component_root[root] = root;
+    dfs_hld(root, -1);
+  }
+
+ private:
+  void dfs_sz(int v, int p) {
+    sub_size[v] = 1;
+    for (auto &u : adj[v]) {
+      if (u == p) continue;
+      dfs_sz(u, v);
+      sub_size[v] += sub_size[u];
+      if (sub_size[u] > sub_size[adj[v][0]]) {
+        std::swap(u, adj[v][0]);
+      }
+    }
+  }
+
+  void dfs_hld(int v, int p) {
+    et_in[v] = counter++;
+    for (auto u : adj[v]) {
+      if (u == p) continue;
+      component_root[u] = (u == adj[v][0] ? component_root[v] : u);
+      dfs_hld(u, v);
+    }
+    et_out[v] = counter;
+  }
+};
