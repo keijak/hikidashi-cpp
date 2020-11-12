704bf8eb112b58fb21ab07c16e7e18194d4c8cc8
Keiji Maekawa <m@keijak.com>
2020-11-12 09:15:02 +0100

    hld

--- src/heavy_light_decomposition.hpp
+++ src/heavy_light_decomposition.hpp
@@ -1,24 +1,27 @@
+#include <functional>
 #include <vector>
 
-// Heavy-Light Decomposition and Euler Tour
+// Heavy-Light Decomposition
 struct HLD {
-  int n;                              // number of nodes in the tree
-  std::vector<std::vector<int>> adj;  // The tree's adjacency list.
-  std::vector<int> subsize;           // subtree size
-  std::vector<int> parent;            // parent node id (or -1)
-  // "index" is preorder index in DFS traversal (Euler Tour).
-  std::vector<int> node_to_index;  // node id to index
-  std::vector<int> index_to_node;  // index to node id
-  std::vector<int> comp_root;      // root of its heavy path component
-  int root;                        // root of the tree
+  using NodeID = int;
 
-  explicit HLD(std::vector<std::vector<int>> g, int root = 0)
+  int n;                                   // number of nodes in the tree
+  std::vector<std::vector<NodeID>> child;  // children node ids
+  std::vector<int> subsize;                // subtree size
+  std::vector<NodeID> parent;              // parent node id (or -1)
+  // "ord" is preorder index in DFS traversal (Euler Tour).
+  std::vector<int> node_to_ord;     // node id to preorder index
+  std::vector<NodeID> ord_to_node;  // preorder index to node id
+  std::vector<NodeID> comp_root;    // root of its heavy path component
+  NodeID root;                      // root of the tree
+
+  explicit HLD(std::vector<std::vector<int>> g, NodeID root = 0)
       : n(int(g.size())),
-        adj(g),
+        child(g),
         subsize(n, 1),
         parent(n, -1),
-        node_to_index(n, -1),
-        index_to_node(n, -1),
+        node_to_ord(n, -1),
+        ord_to_node(n, -1),
         comp_root(n, -1),
         root(root) {
     dfs_subsize(root);
@@ -27,9 +30,10 @@ struct HLD {
     dfs_hld(root, counter);
   }
 
-  int lca(int u, int v) {
+  // Lowest Common Ancestor
+  NodeID lca(NodeID u, NodeID v) {
     for (;;) {
-      if (node_to_index[u] > node_to_index[v]) std::swap(u, v);
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
       if (comp_root[u] == comp_root[v]) return u;
       v = parent[comp_root[v]];
     }
@@ -37,12 +41,11 @@ struct HLD {
 
   // f: [l, r) -> void
   // The intervals contain the indices of both u and v.
-  template <typename F>
-  void for_each(int u, int v, const F &f) {
+  void for_each(NodeID u, NodeID v, std::function<void(int, int)> f) {
     for (;;) {
-      if (node_to_index[u] > node_to_index[v]) std::swap(u, v);
-      f(std::max(node_to_index[comp_root[v]], node_to_index[u]),
-        node_to_index[v] + 1);
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
+      f(std::max(node_to_ord[comp_root[v]], node_to_ord[u]),
+        node_to_ord[v] + 1);
       if (comp_root[u] == comp_root[v]) break;
       v = parent[comp_root[v]];
     }
@@ -51,29 +54,29 @@ struct HLD {
   // f: [l, r) -> void
   // The intervals contain the indices of nodes that represent each edge's
   // deeper end (closer to leaves).
-  template <typename F>
-  void for_each_edge(int u, int v, const F &f) {
+  void for_each_edge(NodeID u, NodeID v, std::function<void(int, int)> f) {
     for (;;) {
-      if (node_to_index[u] > node_to_index[v]) std::swap(u, v);
+      if (node_to_ord[u] > node_to_ord[v]) std::swap(u, v);
       if (comp_root[u] == comp_root[v]) {
-        if (u != v) f(node_to_index[u] + 1, node_to_index[v] + 1);
+        if (u != v) f(node_to_ord[u] + 1, node_to_ord[v] + 1);
         break;
       }
-      f(node_to_index[comp_root[v]], node_to_index[v] + 1);
+      f(node_to_ord[comp_root[v]], node_to_ord[v] + 1);
       v = parent[comp_root[v]];
     }
   }
 
  private:
   // Fills `subsize` and `parent`.
-  void dfs_subsize(int v) {
-    auto &edges = adj[v];
+  void dfs_subsize(NodeID v) {
+    auto &edges = child[v];
     if (parent[v] >= 0) {
-      // Drop the parent from `adj`. It's separately stored in `parent`.
+      // Drop the parent from the `child` adjacency list.
+      // It's separately stored in `parent`.
       auto it = std::find(edges.begin(), edges.end(), parent[v]);
       if (it != edges.end()) edges.erase(it);
     }
-    for (int &u : edges) {
+    for (NodeID &u : edges) {
       parent[u] = v;
       dfs_subsize(u);
       subsize[v] += subsize[u];
@@ -83,13 +86,13 @@ struct HLD {
     }
   }
 
-  // Fills `node_to_index`, `index_to_node`, and `comp_root`.
-  void dfs_hld(int v, int &counter) {
-    node_to_index[v] = counter++;
-    index_to_node[node_to_index[v]] = v;
-    for (int u : adj[v]) {
+  // Fills `node_to_ord`, `ord_to_node`, and `comp_root`.
+  void dfs_hld(NodeID v, int &counter) {
+    node_to_ord[v] = counter++;
+    ord_to_node[node_to_ord[v]] = v;
+    for (NodeID u : child[v]) {
       assert(u != parent[v]);
-      comp_root[u] = (u == adj[v][0] ? comp_root[v] : u);
+      comp_root[u] = (u == child[v][0] ? comp_root[v] : u);
       dfs_hld(u, counter);
     }
   }
