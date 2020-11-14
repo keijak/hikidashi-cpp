279a5057a9a73a8c84919452e4d64573b6c4b07b
Keiji Maekawa <m@keijak.com>
2020-11-14 22:48:28 +0100

    dijkstra

--- src/dijkstra.hpp
+++ src/dijkstra.hpp
@@ -53,26 +53,30 @@ bool operator>(const GridState &x, const GridState &y) {
   return x.cost > y.cost;
 }
 
-std::vector<std::vector<i64>> grid_bfs(const std::vector<std::string> &g,
-                                       int source_r, int source_c) {
+std::vector<std::vector<std::optional<i64>>> grid_bfs(
+    const std::vector<std::string> &g, int source_r, int source_c) {
   const int H = g.size();
   const int W = g[0].size();
   array<int, 4> dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
-  auto mincost = std::vector(H, std::vector(W, INF));
+  auto mincost = std::vector(H, std::vector(W, std::optional<i64>()));
   mincost[source_r][source_c] = 0LL;
   std::deque<GridState> que;
   que.push_back({0LL, source_r, source_c});
   while (not que.empty()) {
     State cur = std::move(que.front());
     que.pop_front();
-    if (cur.cost != mincost[cur.r][cur.c]) continue;
+    if (not mincost[cur.r][cur.c].has_value() or
+        mincost[cur.r][cur.c].value() != cur.cost) {
+      continue;
+    }
     for (int i = 0; i < 4; ++i) {
       int nr = cur.r + dy[i];
       int nc = cur.c + dx[i];
       if (nr < 0 or nr >= H or nc < 0 or nc >= W) continue;
       if (g[nr][nc] == '#') continue;
       i64 new_cost = cur.cost + 1;
-      if (mincost[nr][nc] > new_cost) {
+      if (not mincost[nr][nc].has_value() or
+          mincost[nr][nc].value() > new_cost) {
         mincost[nr][nc] = new_cost;
         que.push_back({new_cost, nr, nc});
       }
