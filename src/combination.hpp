d24aaae9e181b835bf1c8a0ebd7b3f3855023570
Keiji Maekawa <m@keijak.com>
2020-11-25 01:29:08 +0100

    merged factorial and combination

--- src/combination.hpp
+++ src/combination.hpp
@@ -42,3 +42,12 @@ vector<vector<T>> comb_table(int n) {
   }
   return C;
 }
+
+Mint factorial(int x) {
+  static std::vector<Mint> facts = {1, 1, 2};
+  facts.reserve(x + 1);
+  while (facts.size() <= x) {
+    facts.push_back(facts.back() * facts.size());
+  }
+  return facts[x];
+}
