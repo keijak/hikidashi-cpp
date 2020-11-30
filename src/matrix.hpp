bf445ad8d457586c81c1c3c94cc1cb2164c8452d
Keiji Maekawa <m@keijak.com>
2020-11-30 10:33:46 +0100

    matrix

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -1,7 +1,7 @@
-template <class T>
+template <class T = Mint>
 struct Matrix {
-  using Row = vector<T>;
-  vector<Row> A;
+  using Row = std::vector<T>;
+  std::vector<Row> A;
 
   Matrix() {}
 
@@ -9,46 +9,70 @@ struct Matrix {
 
   Matrix(int n) : A(n, Row(n, 0)){};
 
-  int height() const { return (int)(A.size()); }
+  inline int height() const { return (int)(A.size()); }
 
-  int width() const { return (int)(A[0].size()); }
+  inline int width() const { return (int)(A[0].size()); }
 
-  inline const Row &operator[](int k) const { return (A.at(k)); }
+  inline const Row &operator[](int k) const { return A[k]; }
 
-  inline Row &operator[](int k) { return (A.at(k)); }
+  inline Row &operator[](int k) { return A[k]; }
 
   static Matrix I(int n) {
     Matrix mat(n);
     for (int i = 0; i < n; i++) mat[i][i] = 1;
-    return (mat);
+    return mat;
+  }
+
+  static Matrix row(std::vector<T> v) {
+    Matrix mat(1, v.size());
+    mat[0] = std::move(v);
+    return mat;
+  }
+
+  static Matrix column(const std::vector<T> &v) {
+    const int n = v.size();
+    Matrix mat(n, 1);
+    for (int i = 0; i < n; ++i) mat[i][0] = v[i];
+    return mat;
+  }
+
+  Matrix transpose() const {
+    int n = height(), m = width();
+    Matrix mat(m, n);
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < m; ++j) {
+        mat[j][i] = (*this)[i][j];
+      }
+    }
+    return mat;
   }
 
   Matrix &operator+=(const Matrix &B) {
-    size_t n = height(), m = width();
-    assert(n == B.height() && m == B.width());
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++) (*this)[i][j] += B[i][j];
-    return (*this);
+    int n = height(), m = width();
+    assert(n == B.height() and m == B.width());
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < m; ++j) (*this)[i][j] += B[i][j];
+    return *this;
   }
 
   Matrix &operator-=(const Matrix &B) {
-    size_t n = height(), m = width();
-    assert(n == B.height() && m == B.width());
-    for (int i = 0; i < n; i++)
-      for (int j = 0; j < m; j++) (*this)[i][j] -= B[i][j];
-    return (*this);
+    int n = height(), m = width();
+    assert(n == B.height() and m == B.width());
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < m; ++j) (*this)[i][j] -= B[i][j];
+    return *this;
   }
 
   Matrix &operator*=(const Matrix &B) {
     int n = height(), m = B.width(), p = width();
     assert(p == B.height());
-    vector<Row> C(n, Row(m, 0));
+    std::vector<Row> C(n, Row(m, 0));
     for (int i = 0; i < n; i++)
       for (int j = 0; j < m; j++)
         for (int k = 0; k < p; k++)
           C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
-    A.swap(C);
-    return (*this);
+    std::swap(A, C);
+    return *this;
   }
 
   Matrix &operator^=(long long k) {
@@ -59,7 +83,7 @@ struct Matrix {
       k >>= 1LL;
     }
     A.swap(B.A);
-    return (*this);
+    return *this;
   }
 
   Matrix operator+(const Matrix &B) const { return (Matrix(*this) += B); }
@@ -70,15 +94,14 @@ struct Matrix {
 
   Matrix operator^(const long long k) const { return (Matrix(*this) ^= k); }
 
-  friend ostream &operator<<(ostream &os, Matrix &p) {
-    size_t n = p.height(), m = p.width();
+  Matrix pow(const long long k) const { return (*this) ^ k; }
+
+  friend std::ostream &operator<<(std::ostream &os, const Matrix &p) {
+    int n = p.height();
     for (int i = 0; i < n; i++) {
-      os << "[";
-      for (int j = 0; j < m; j++) {
-        os << p[i][j] << (j + 1 == m ? "]\n" : ",");
-      }
+      os << p[i] << "\n";
     }
-    return (os);
+    return os;
   }
 
   T det() {
@@ -90,7 +113,7 @@ struct Matrix {
       for (int j = i; j < width(); j++) {
         if (B[j][i] != 0) idx = j;
       }
-      if (idx == -1) return (0);
+      if (idx == -1) return 0;
       if (i != idx) {
         ret *= -1;
         swap(B[i], B[idx]);
@@ -107,6 +130,6 @@ struct Matrix {
         }
       }
     }
-    return (ret);
+    return ret;
   }
 };
