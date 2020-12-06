08b788ae52d7ecfaf48a6bbd0b293e4c92e43c37
Keiji Maekawa <m@keijak.com>
2020-12-06 23:23:00 +0100

    closed interval set

--- src/interval_set.hpp
+++ src/interval_set.hpp
@@ -18,7 +18,7 @@ struct IntervalSet {
 
   int count() const { return set_.size(); }
 
-  const PSet &get() const { return set_; }
+  const PSet& get() const { return set_; }
 
   // Adds an interval. It's merged with all existing intervals.
   // [istart, iend) - right-open interval
@@ -64,4 +64,56 @@ struct IntervalSet {
 
   // Returns true if the point is included in an interval in the set.
   bool contains(i64 point) const { return this->find(point) != this->end(); }
+};
+
+// Set of disjoint closed intervals [l, r].
+class ClosedIntervalSet : public std::map<i64, i64> {
+ private:
+  // If true, automatically merges [l, c] and [c+1, r].
+  bool merge_adjacent;
+
+ public:
+  explicit ClosedIntervalSet(bool merge_adjacent)
+      : merge_adjacent(merge_adjacent) {}
+
+  // Returns the interval [l, r] which contains p if available.
+  // Otherwise returns this->end().
+  std::map<i64, i64>::iterator get(i64 p) const {
+    auto it = upper_bound(p);
+    if (it == begin() || (--it)->second < p) return this->end();
+    return it;
+  }
+
+  // Inserts interval [l, r]
+  void insert(i64 l, i64 r) {
+    auto itl = upper_bound(l), itr = upper_bound(r + merge_adjacent);
+    if (itl != begin()) {
+      if ((--itl)->second < l - merge_adjacent) ++itl;
+    }
+    if (itl != itr) {
+      l = std::min(l, itl->first);
+      r = std::max(r, std::prev(itr)->second);
+      erase(itl, itr);
+    }
+    (*this)[l] = r;
+  }
+
+  // Removes interval [l, r]
+  void remove(i64 l, i64 r) {
+    auto itl = upper_bound(l), itr = upper_bound(r);
+    if (itl != begin()) {
+      if ((--itl)->second < l) ++itl;
+    }
+    if (itl == itr) return;
+    int tl = std::min(l, itl->first), tr = std::max(r, std::prev(itr)->second);
+    erase(itl, itr);
+    if (tl < l) (*this)[tl] = l - 1;
+    if (r < tr) (*this)[r + 1] = tr;
+  }
+
+  // Are p and q in the same interval?
+  bool same(i64 p, i64 q) const {
+    const auto& it = get(p);
+    return it != end() && it->first <= q && q <= it->second;
+  }
 };
