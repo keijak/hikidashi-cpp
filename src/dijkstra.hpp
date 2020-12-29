a617065fe257a6008ecbbe54cb037ac25f0a55bf
Keiji Maekawa <m@keijak.com>
2020-12-29 17:37:34 +0100

    simplified dijkstra

--- src/dijkstra.hpp
+++ src/dijkstra.hpp
@@ -2,8 +2,10 @@
 using namespace std;
 using i64 = long long;
 
+const i64 INF = 1e16;
+
 template <class T>
-using MinHeap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+using MinHeap = priority_queue<T, vector<T>, greater<T>>;
 
 struct Edge {
   i64 cost;
@@ -17,23 +19,19 @@ struct State {
 bool operator>(const State &x, const State &y) { return x.cost > y.cost; }
 
 // Returns min distance from the source node to each node (if exists).
-std::vector<std::optional<i64>> dijkstra(
-    const std::vector<std::vector<Edge>> &g, int source) {
+vector<i64> dijkstra(const vector<vector<Edge>> &g, int source) {
   const int n = g.size();
-  std::vector<std::optional<i64>> mincost(n);
+  vector<i64> mincost(n, INF);
   mincost[source] = 0LL;
   MinHeap<State> que;
   que.push({0, source});
   while (not que.empty()) {
-    State cur = std::move(que.top());
+    State cur = move(que.top());
     que.pop();
-    if (not mincost[cur.node].has_value() or
-        cur.cost != mincost[cur.node].value()) {
-      continue;
-    }
+    if (cur.cost > mincost[cur.node]) continue;
     for (const auto &e : g[cur.node]) {
       auto new_cost = cur.cost + e.cost;
-      if (not mincost[e.to].has_value() or mincost[e.to].value() > new_cost) {
+      if (mincost[e.to] > new_cost) {
         mincost[e.to] = new_cost;
         que.push({new_cost, e.to});
       }
@@ -42,9 +40,8 @@ std::vector<std::optional<i64>> dijkstra(
   return mincost;
 }
 
-std::vector<std::optional<i64>> grid_search(const std::vector<std::string> &g,
-                                            int source_r, int source_c) {
-  const i64 INF = 1e18;
+vector<vector<i64>> grid_search(const vector<string> &g, int source_r,
+                                int source_c) {
   const int H = g.size();
   const int W = g[0].size();
   auto pack = [&](int r, int c) -> int { return r * W + c; };
@@ -52,28 +49,25 @@ std::vector<std::optional<i64>> grid_search(const std::vector<std::string> &g,
     return {node / W, node % W};
   };
   array<int, 4> dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
-  auto mincost = std::vector(H * W, std::optional<i64>());
+  vector mincost(H, vector(W, INF));
+  mincost[source_r][source_c] = 0LL;
   int source_node = pack(source_r, source_c);
-  mincost[source_node] = 0LL;
-  std::queue<State> que;
+  queue<State> que;
   que.push({0LL, source_node});
   while (not que.empty()) {
-    State cur = std::move(que.front());
+    State cur = move(que.front());
     que.pop();
-    if (not mincost[cur.node].has_value() or
-        mincost[cur.node].value() != cur.cost) {
-      continue;
-    }
     auto [cur_r, cur_c] = unpack(cur.node);
+    if (cur.cost > mincost[cur_r][cur_c]) continue;
     for (int i = 0; i < 4; ++i) {
-      int nr = cur_r + dy[i];
-      int nc = cur_c + dx[i];
+      int nr = cur_r + dx[i];
+      int nc = cur_c + dy[i];
       if (nr < 0 or nr >= H or nc < 0 or nc >= W) continue;
       if (g[nr][nc] == '#') continue;
       int new_node = pack(nr, nc);
       i64 new_cost = cur.cost + 1;
-      if (mincost[new_node].value_or(INF) > new_cost) {
-        mincost[new_node] = new_cost;
+      if (new_cost < mincost[nr][nc]) {
+        mincost[nr][nc] = new_cost;
         que.push({new_cost, new_node});
       }
     }
