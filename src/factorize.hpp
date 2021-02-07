8ba396276562f8818dbce4b398194c5e4e1dd442
Keiji Maekawa <m@keijak.com>
2021-02-07 13:21:05 +0100

    sieve

--- src/factorize.hpp
+++ src/factorize.hpp
@@ -37,9 +37,10 @@ std::vector<bool> prime_sieve(int n) {
   std::vector<bool> is_prime(n + 1, true);
   is_prime[0] = is_prime[1] = false;
   for (int i = 4; i <= n; i += 2) is_prime[i] = false;
-  for (int i = 3; i * i <= n; i += 2) {
-    if (!is_prime[i]) continue;
-    for (int j = 2 * i; j <= n; j += i) is_prime[j] = false;
+  int sqrt_n = std::lround(std::ceil(std::sqrt((double)n)));  // n in 32 bits.
+  for (int i = 3; i <= sqrt_n; i += 2) {
+    if (not is_prime[i]) continue;
+    for (int j = i * i; j <= n; j += i) is_prime[j] = false;
   }
   return is_prime;
 }
@@ -50,9 +51,10 @@ std::vector<int> min_divisor_sieve(int n) {
   std::vector<int> res(n + 1);
   for (int i = 1; i <= n; ++i) res[i] = i;
   for (int i = 4; i <= n; i += 2) res[i] = 2;
-  for (int i = 3; i * i <= n; i += 2) {
+  int sqrt_n = std::lround(std::ceil(std::sqrt((double)n)));  // n in 32 bits.
+  for (int i = 3; i <= sqrt_n; i += 2) {
     if (res[i] != i) continue;
-    for (int j = 2 * i; j <= n; j += i) {
+    for (int j = i * i; j <= n; j += i) {
       if (res[j] == j) res[j] = i;
     }
   }
