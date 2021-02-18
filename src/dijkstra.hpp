11ce95f5641bb25a86cd07864fec207991d6eaa7
Keiji Maekawa <m@keijak.com>
2021-02-18 22:17:49 +0100

    dijkstra

--- src/dijkstra.hpp
+++ src/dijkstra.hpp
@@ -23,55 +23,62 @@ struct State {
 bool operator>(const State &x, const State &y) { return x.cost > y.cost; }
 
 // Returns min distance from the source node to each node (if exists).
-vector<i64> dijkstra(const vector<vector<Edge>> &g, int source) {
+auto search(const vector<vector<Edge>> &g, int source) {
   const int n = g.size();
   vector<i64> mincost(n, INF);
-  mincost[source] = 0LL;
   MinHeap<State> que;
-  que.push({0, source});
+  auto push = [&](i64 cost, int node) -> bool {
+    if (chmin(mincost[node], cost)) {
+      que.push({cost, node});
+      return true;
+    }
+    return false;
+  };
+  assert(push(0LL, source));
+
   while (not que.empty()) {
     State cur = move(que.top());
     que.pop();
-    if (cur.cost > mincost[cur.node]) continue;
+    if (cur.cost != mincost[cur.node]) {
+      continue;
+    }
     for (const auto &e : g[cur.node]) {
-      auto new_cost = cur.cost + e.cost;
-      if (chmin(mincost[e.to], new_cost)) {
-        que.push({new_cost, e.to});
-      }
+      i64 new_cost = cur.cost + e.cost;
+      push(new_cost, e.to);
     }
   }
   return mincost;
 }
 
-vector<vector<i64>> grid_search(const vector<string> &g, int source_r,
-                                int source_c) {
+auto grid_search(const vector<string> &g, int source_r, int source_c) {
   const int H = g.size();
   const int W = g[0].size();
-  auto pack = [&](int r, int c) -> int { return r * W + c; };
-  auto unpack = [&](int node) -> pair<int, int> {
-    return {node / W, node % W};
-  };
   array<int, 4> dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
   vector mincost(H, vector(W, INF));
-  mincost[source_r][source_c] = 0LL;
-  int source_node = pack(source_r, source_c);
   queue<State> que;
-  que.push({0LL, source_node});
+  auto push = [&](i64 cost, int r, int c) -> bool {
+    if (r < 0 or r >= H or c < 0 or c >= W) return false;
+    if (g[r][c] == '#') return false;
+    if (not chmin(mincost[r][c], cost)) return false;
+    int node = r * W + c;
+    que.push({cost, node});
+    return true;
+  };
+  assert(push(0LL, source_r, source_c));
+
   while (not que.empty()) {
     State cur = move(que.front());
     que.pop();
-    auto [cur_r, cur_c] = unpack(cur.node);
-    if (cur.cost > mincost[cur_r][cur_c]) continue;
+    int cur_r = cur.node / W;
+    int cur_c = cur.node % W;
+    if (cur.cost != mincost[cur_r][cur_c]) {
+      continue;
+    }
     for (int i = 0; i < 4; ++i) {
       int nr = cur_r + dx[i];
       int nc = cur_c + dy[i];
-      if (nr < 0 or nr >= H or nc < 0 or nc >= W) continue;
-      if (g[nr][nc] == '#') continue;
-      int new_node = pack(nr, nc);
       i64 new_cost = cur.cost + 1;
-      if (chmin(mincost[nr][nc], new_cost)) {
-        que.push({new_cost, new_node});
-      }
+      push(new_cost, nr, nc);
     }
   }
   return mincost;
