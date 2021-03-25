c3b426f0f0894db319b08c600572dab0f0719db0
keijak <m@keijak.com>
2021-03-25 13:14:45 +0100

    gp_hash_table resize

--- src/gnu_pbds.hpp
+++ src/gnu_pbds.hpp
@@ -11,5 +11,13 @@ using rb_tree_map =
     __gnu_pbds::tree<Key, T, std::less<Key>, __gnu_pbds::rb_tree_tag,
                      __gnu_pbds::tree_order_statistics_node_update>;
 
+// Enables `__gnu_pbds::gp_hash_table::resize()` similar to
+// `std::unordered_map::reserve()`.
 // #include <ext/pb_ds/assoc_container.hpp>
-using __gnu_pbds::gp_hash_table;
+template <class K, class V>
+using gp_hash_table = __gnu_pbds::gp_hash_table<
+    K, V, std::hash<K>, std::equal_to<K>,
+    __gnu_pbds::direct_mask_range_hashing<>, __gnu_pbds::linear_probe_fn<>,
+    __gnu_pbds::hash_standard_resize_policy<
+        __gnu_pbds::hash_exponential_size_policy<>,
+        __gnu_pbds::hash_load_check_resize_trigger<>, true> >;
