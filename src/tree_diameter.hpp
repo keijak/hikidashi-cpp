82c50256c5e7c07524c572407f7a845d2dfe9592
keijak <m@keijak.com>
2021-04-01 10:14:17 +0200

    cleanup

--- src/tree_diameter.hpp
+++ src/tree_diameter.hpp
@@ -1,21 +1,23 @@
+#include <vector>
+
 // Tree diameter.
 // Returns the maximum diameter and two end point nodes with the diameter.
-pair<int, pair<int, int>> tree_diameter(const V<V<int>>& adj) {
-  auto dfs = [&](auto self, int v, int p, int d) -> pair<int, int> {
+std::pair<int, std::pair<int, int>> tree_diameter(
+    const std::vector<std::vector<int>>& g) {
+  auto farthest = [&](auto& rec, int v, int p, int d) -> std::pair<int, int> {
     int res_d = d, res_v = v;
-    for (auto u : adj[v]) {
-      if (u != p) {
-        auto [child_d, child_v] = self(self, u, v, d + 1);
-        if (child_d > res_d) {
-          res_d = child_d;
-          res_v = child_v;
-        }
+    for (auto u : g[v]) {
+      if (u == p) continue;
+      const auto& [child_d, child_v] = rec(rec, u, v, d + 1);
+      if (child_d > res_d) {
+        res_d = child_d;
+        res_v = child_v;
       }
     }
     return {res_d, res_v};
   };
 
-  auto [d1, u] = dfs(dfs, 0, -1, 0);
-  auto [d2, v] = dfs(dfs, u, -1, 0);
-  return {d2, make_pair(u, v)};
+  const auto& [_, u] = farthest(farthest, 0, -1, 0);
+  const auto& [distance, v] = farthest(farthest, u, -1, 0);
+  return std::pair{distance, std::pair(u, v)};
 }
