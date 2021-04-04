2465ec73a009d9025b45245cf89167d8d0a7138c
Keiji Maekawa <m@keijak.com>
2021-04-04 19:52:14 +0200

    bell number

--- src/combination.hpp
+++ src/combination.hpp
@@ -22,28 +22,6 @@ struct Factorials {
   }
 };
 
-// Permutation (nPk)
-template <class T>
-T permutation(const Factorials<T>& fs, int n, int k) {
-  if (k < 0 || k > n) return 0;
-  return fs.fact[n] * fs.ifact[n - k];
-}
-
-// Total number of complete permutations of size n.
-template <class T>
-T montmort_number(const Factorials<T>& fs, int n) {
-  T res = 0;
-  for (int k = 2; k <= n; ++k) {
-    if (k % 2 == 0) {
-      res += fs.ifact[k];
-    } else {
-      res -= fs.ifact[k];
-    }
-  }
-  res *= fs.fact[n];
-  return res;
-}
-
 // C[n][k] = nCk
 // Cretates a combination matrix using Pascal's triangle.
 // Time and space complexity: Θ(n^2)
@@ -84,10 +62,56 @@ Mint factorial(int x) {
   return facts[x];
 }
 
+// Permutation (nPk)
+template <class T>
+T permutation(const Factorials<T>& fs, int n, int k) {
+  if (k < 0 || k > n) return 0;
+  return fs.fact[n] * fs.ifact[n - k];
+}
+
 // Catalan Number
 template <typename T>
 T catalan(const Factorials<T>& fs, int k) {
   auto ret = fs.C(2 * k, k);
   if (k > 0) ret -= fs.C(2 * k, k - 1);
+  return ret;
+}
+
+// Montmort Number
+// Total number of complete permutations of size n.
+template <class T>
+T montmort_number(const Factorials<T>& fs, int n) {
+  T res = 0;
+  for (int k = 2; k <= n; ++k) {
+    if (k % 2 == 0) {
+      res += fs.ifact[k];
+    } else {
+      res -= fs.ifact[k];
+    }
+  }
+  res *= fs.fact[n];
+  return res;
+}
+
+// Bell Number B(n, k)
+// Number of ways of grouping n distinguishable balls into k indistinguishable
+// boxes.
+template <typename T>
+T bell_number(const Factorials<T>& fs, int n, int k) {
+  if (n == 0) return 1;
+  k = std::min(k, n);
+  T ret = 0;
+  std::vector<T> p(k + 1);
+  p[0] = 1;
+  for (int i = 1; i <= k; i++) {
+    if (i & 1) {
+      p[i] = p[i - 1] - fs.ifact(i);
+    } else {
+      p[i] = p[i - 1] + fs.ifact(i);
+    }
+  }
+  for (int i = 1; i <= k; i++) {
+    ret += T(i).pow(n) * fs.ifact(i) * p[k - i];
+  }
   return ret;
 }
