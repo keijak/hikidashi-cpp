094c6858de206d45ba7c7c2777ce5dcb8714699b
keijak <m@keijak.com>
2021-04-29 20:43:32 +0200

    burn-or-bury solver

--- src/project_selection_problem.hpp
+++ src/project_selection_problem.hpp
@@ -2,11 +2,11 @@
 
 #include <atcoder/maxflow>
 
+const int BIG_COST = 1e8;
+
 template <typename T>
 struct ProjectSelection {
  private:
-  const int BIG_COST = 100000;
-
   // Assign Color 1 to kSource and Color 0 to kSink.
   const int kSource, kSink;
   const int n_;  // number of initial nodes
@@ -24,6 +24,52 @@ struct ProjectSelection {
  public:
   explicit ProjectSelection(int n)
       : kSource(n), kSink(n + 1), n_(n), m_(0), bonus_(0) {}
+
+  T min_cost() const {
+    atcoder::mf_graph<T> g(n_ + m_ + 2);
+    for (const Edge &e : edges_) {
+      g.add_edge(e.from, e.to, e.cost);
+    }
+    return g.flow(kSource, kSink) - bonus_;
+  }
+
+  // === High-level constraint API ===
+
+  void constraint(int x, int y, T cost) {
+    if (x >= 0 and y < 0) {
+      assert(cost >= 0);
+      X_and_notY_lose(x, ~y, cost);
+    } else if (x < 0 and y >= 0) {
+      assert(cost >= 0);
+      X_and_notY_lose(y, ~x, cost);
+    } else if (x >= 0 and y >= 0) {
+      assert(cost <= 0);
+      all_of_X_gain({x, y}, -cost);
+    } else if (x < 0 and y < 0) {
+      assert(cost <= 0);
+      none_of_X_gain({~x, ~y}, -cost);
+    } else {
+      assert(false);
+    }
+  }
+
+  void constraint(int x, T cost) {
+    if (x >= 0) {
+      if (cost >= 0) {
+        X_lose(x, cost);
+      } else {
+        X_gain(x, -cost);
+      }
+    } else {
+      if (cost >= 0) {
+        notX_lose(~x, cost);
+      } else {
+        notX_gain(~x, cost);
+      }
+    }
+  }
+
+  // === Low-level constraint API ===
 
   // {X=1, Y=0} => penalty
   void X_and_notY_lose(int x, int y, T penalty) {
@@ -48,14 +94,8 @@ struct ProjectSelection {
     X_lose(x, revenue);
   }
 
-  // {X=0,Y=1} or {X=1,Y=0} => penalty
-  void X_xor_Y_lose(int x, int y, T penalty) {
-    X_and_notY_lose(x, y, penalty);
-    X_and_notY_lose(y, x, penalty);
-  }
-
   // {Xi=1 forall i} => revenue
-  void all_of_X_gain(vector<int> x, T revenue) {
+  void all_of_X_gain(std::vector<int> x, T revenue) {
     const int y = n_ + 2 + m_++;
     bonus_ += revenue;
     X_and_notY_lose(kSource, y, revenue);
@@ -65,7 +105,7 @@ struct ProjectSelection {
   }
 
   // {Xi=0 forall i} => revenue
-  void none_of_X_gain(vector<int> x, T revenue) {
+  void none_of_X_gain(std::vector<int> x, T revenue) {
     const int y = n_ + 2 + m_++;
     bonus_ += revenue;
     X_and_notY_lose(y, kSink, revenue);
@@ -80,13 +120,9 @@ struct ProjectSelection {
     none_of_X_gain({x, y}, revenue);
   }
 
-  T min_cost() const {
-    atcoder::mf_graph<T> g(n_ + m_ + 2);
-    for (const Edge &e : edges_) {
-      g.add_edge(e.from, e.to, e.cost);
-    }
-    return g.flow(kSource, kSink);
+  // {X=0,Y=1} or {X=1,Y=0} => penalty
+  void X_not_equal_Y_lose(int x, int y, T penalty) {
+    X_and_notY_lose(x, y, penalty);
+    X_and_notY_lose(y, x, penalty);
   }
-
-  T max_revenue() const { return bonus_ - min_cost(); }
 };
