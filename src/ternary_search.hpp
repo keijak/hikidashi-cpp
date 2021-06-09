54fb7fb2e9246df1b3b2b5f97fb60939ad78f2f3
keijak <m@keijak.com>
2021-06-09 10:44:13 +0200

    no std::function

--- src/ternary_search.hpp
+++ src/ternary_search.hpp
@@ -1,5 +1,25 @@
 #include <bits/stdc++.h>
 using i64 = long long;
+using Float = double;
+
+// Ternary search over a real-valued open interval.
+// Finds x in (low, high) s.t. f(x) is minimal.
+template <class F, class T = std::invoke_result_t<F, Float>>
+Float find_min_ternary_search(Float low, Float high, F f) {
+  static_assert(std::is_invocable_v<F, Float>);
+  std::less<T> compare;  // Change it to greater<T> if you want max.
+  Float l = low, r = high;
+  for (int iter = 0; iter < 100; ++iter) {
+    auto ll = (l + l + r) / 3.0;
+    auto rr = (l + r + r) / 3.0;
+    if (compare(f(ll), f(rr))) {
+      r = rr;
+    } else {
+      l = ll;
+    }
+  }
+  return (l + r) / 2.0;
+}
 
 // Ternary search over an integer interval.
 // Finds x in [low, high) s.t. f(x) is minimal.
@@ -7,10 +27,11 @@ using i64 = long long;
 // Note: If the input range size is 1 (e.g. [1, 2)), this function returns the
 // result without evaluating `f(x)` at all. To get the minimal value instead of
 // the argmin, memoise `f` and call `f(result)`.
-template <class T, class Compare = std::less<T>,
-          class F = std::function<T(i64)>>
+template <class F, class T = std::invoke_result_t<F, i64>>
 i64 find_min_ternary_search(i64 low, i64 high, F f) {
-  Compare compare;
+  static_assert(std::is_invocable_v<F, i64>);
+  std::less<T> compare;  // Change it to greater<T> if you want max.
+
   --low;  // Make it an open interval: (low, high).
   i64 l = low, r = high;
   while (r - l > 2) {
@@ -28,10 +49,11 @@ i64 find_min_ternary_search(i64 low, i64 high, F f) {
 // Golden Section Search over an integer interval.
 // Finds x in [low, high) s.t. func(x) is minimal.
 // Unlike ternary search, `f(x)` is not required to be convex.
-template <class T, class Compare = std::less<T>,
-          class F = std::function<T(i64)>>
+template <class F, class T = std::invoke_result_t<F, i64>>
 i64 find_min_golden_section_search(i64 low, i64 high, F f) {
-  Compare compare;
+  static_assert(std::is_invocable_v<F, i64>);
+  std::less<T> compare;  // Change it to greater<T> if you want max.
+
   --low;             // Make it an open interval: (low, high).
   i64 l = 1, r = 1;  // Left and right offsets from `low`.
   while (l + r < high - low) {
