7713722dd29882407b6d83a518ca3dc93be00b6d
keijak <m@keijak.com>
2021-06-11 14:13:26 +0200

    infi

--- src/infinity.hpp
+++ src/infinity.hpp
@@ -5,9 +5,13 @@ template <int sign = 1>
 struct Infinity {
   template <typename T>
   constexpr operator T() const {
-    static_assert(sign == 1 or not std::is_unsigned<T>::value,
+    static_assert(sign == 1 or not std::is_unsigned_v<T>,
                   "must be positive in an unsigned type");
-    return (std::numeric_limits<T>::max() / T(2)) * T(sign);
+    if constexpr (std::numeric_limits<T>::has_infinity) {
+      return T(sign) * std::numeric_limits<T>::infinity();
+    } else {
+      return T(sign) * (std::numeric_limits<T>::max() / T(2));
+    }
   }
   constexpr Infinity<sign * -1> operator-() const { return {}; }
   template <typename T>
