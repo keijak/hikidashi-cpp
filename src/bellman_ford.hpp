3dab9294fee01a6438e70ebcce1307f45a0d2054
keijak <m@keijak.com>
2021-06-22 12:34:12 +0200

    bellman ford

--- src/bellman_ford.hpp
+++ src/bellman_ford.hpp
@@ -1,48 +1,56 @@
-#include <unordered_set>
+#include <limits>
 #include <vector>
 
 using i64 = long long;
 
-const i64 INF = 1e18;
+const i64 INF = std::numeric_limits<i64>::max() / 2;
 
-// Finds the shortest path from the start node and detects negative cycle.
-// Returns the distance from the start node to each node.
-// INF indicates unreachable. -INF indicates having a negative cycle in a
-// path from the start node.
-std::vector<i64> bellman_ford(
-    const std::vector<std::vector<std::pair<int, i64>>> &adj, const int start) {
+struct Edge {
+  i64 cost;
+  int to;
+};
+
+// Finds the shortest path from the start node and detects negative cycle if
+// exists. Returns the minimum cost from the start node to each node. INF
+// indicates unreachable. -INF indicates having a negative cycle in a path from
+// the start node.
+auto bellman_ford(const std::vector<std::vector<Edge>>& adj, const int start) {
   const int n = int(adj.size());
-  const i64 INF = 1e18;
-  std::vector<i64> min_dist(n, INF);  // distance from the start node.
-  min_dist[start] = 0;
+  std::vector<i64> mincost(n, INF);
+  mincost[start] = 0;
 
   for (int k = 0; k < n - 1; ++k) {
     for (int i = 0; i < n; ++i) {
-      const auto di = min_dist[i];
-      if (di == INF) continue;  // We haven't reached i yet.
-      for (auto [j, dist] : adj[i]) {
-        if (min_dist[j] > di + dist) {
-          min_dist[j] = di + dist;
+      const i64 di = mincost[i];
+      if (di == INF) continue;  // Haven't reached i yet.
+      for (const Edge& e : adj[i]) {
+        if (mincost[e.to] > di + e.cost) {
+          mincost[e.to] = di + e.cost;
         }
       }
     }
   }
 
   // Negative cycle detection.
-  // When there's no negative cycle, at least one node gets the shortest
-  // distance determined in each iteration. If we have gone through N+1
+  // If there's no negative cycle, at least one node gets the shortest
+  // distance determined in each iteration above. If we have gone through N-1
   // iterations and still have an update, there must be a negative cycle.
   for (int k = 0; k < n; ++k) {
+    bool updated = false;
     for (int i = 0; i < n; ++i) {
-      if (min_dist[i] == INF) continue;
-      const bool negative_cycle = (min_dist[i] == -INF);
-      for (auto [j, dist] : adj[i]) {
-        if (negative_cycle or min_dist[j] > min_dist[i] + dist) {
-          min_dist[j] = -INF;
+      const i64 di = mincost[i];
+      if (di == INF) continue;
+      const bool in_negative_cycle = (di == -INF);
+      for (const Edge& e : adj[i]) {
+        if (mincost[e.to] == -INF) continue;
+        if (in_negative_cycle or (mincost[e.to] > di + e.cost)) {
+          mincost[e.to] = -INF;
+          updated = true;
         }
       }
     }
+    if (not updated) break;
   }
 
-  return min_dist;
+  return mincost;
 }
