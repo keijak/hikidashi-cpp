464cafbcff02580310b9bd61667d1d12e8d57767
keijak <m@keijak.com>
2021-07-04 01:57:59 +0200

    binary trie

---
+++ src/binary_trie.hpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+
+template <typename T = unsigned, int kBitWidth = std::numeric_limits<T>::digits>
+struct BinaryTrie {
+  static_assert(std::is_unsigned<T>::value, "Requires unsigned type");
+
+ public:
+  struct Node;
+  using NodePtr = Node *;
+
+  struct Node {
+    int leaf_count;
+    std::array<NodePtr, 2> child;
+    mutable T lazy_mask;
+    Node() : leaf_count(0), child{nullptr, nullptr}, lazy_mask(0) {}
+  };
+  NodePtr root_;  // The root node.
+
+  BinaryTrie() : root_(nullptr) {}
+
+  int size() const { return root_ ? root_->leaf_count : 0; }
+
+  bool empty() const { return size() == 0; }
+
+  void insert(T val) { root_ = insert_internal(root_, val); }
+
+  // Removes one element of `val`.
+  // At least one `val` must exist in the trie. Check `trie.count(val) > 0`.
+  void erase_one(T val) { root_ = erase_internal(root_, val); }
+
+  // Returns the element x in the trie that maximizes `x ^ xor_mask`.
+  T max_element(T xor_mask = 0) const { return get_min(root_, ~xor_mask); }
+
+  // Returns the element x in the trie that minimizes `x ^ xor_mask`.
+  T min_element(T xor_mask = 0) const { return get_min(root_, xor_mask); }
+
+  // Returns the minimum index i s.t. trie[i] >= val.
+  int lower_bound(T val) const { return count_less(root_, val); }
+
+  // Returns the minimum index i s.t. trie[i] > val.
+  int upper_bound(T val) const { return count_less(root_, val + 1); }
+
+  // Returns k-th (0-indexed) smallest value.
+  T operator[](int k) const {
+    assert(0 <= k && k < size());
+    return get_internal(root_, k);
+  }
+
+  // Returns k-th (0-indexed) largest value.
+  T kth_largest(int k) const {
+    const int i = size() - k - 1;
+    return (*this)[i];
+  }
+
+  // Counts the number of elements that are equal to `val`.
+  // Note: BinaryTrie is a multiset.
+  int count(T val) const {
+    if (!root_) return 0;
+    NodePtr t = root_;
+    for (int i = kBitWidth - 1; i >= 0; i--) {
+      t = t->child[(val >> T(i)) & 1];
+      if (!t) return 0;
+    }
+    return t->leaf_count;
+  }
+
+  void xor_all(T xor_mask) {
+    if (root_) root_->lazy_mask ^= xor_mask;
+  }
+
+ private:
+  void push_down(NodePtr t, int b) const {
+    if (t->lazy_mask == 0) return;
+    if ((t->lazy_mask >> b) & 1) std::swap(t->child[0], t->child[1]);
+    if (t->child[0]) t->child[0]->lazy_mask ^= t->lazy_mask;
+    if (t->child[1]) t->child[1]->lazy_mask ^= t->lazy_mask;
+    t->lazy_mask = 0;
+  }
+
+  NodePtr insert_internal(NodePtr t, T val, int b = kBitWidth - 1) {
+    if (!t) t = new Node();
+    t->leaf_count += 1;
+    if (b < 0) return t;
+    push_down(t, b);
+    bool f = (val >> b) & 1;
+    t->child[f] = insert_internal(t->child[f], val, b - 1);
+    return t;
+  }
+
+  NodePtr erase_internal(NodePtr t, T val, int b = kBitWidth - 1) {
+    assert(t);
+    t->leaf_count -= 1;
+    if (t->leaf_count == 0) {
+      delete t;
+      return nullptr;
+    }
+    if (b < 0) return t;
+    push_down(t, b);
+    bool f = (val >> b) & 1;
+    t->child[f] = erase_internal(t->child[f], val, b - 1);
+    return t;
+  }
+
+  T get_min(NodePtr t, T xor_mask, int b = kBitWidth - 1) const {
+    assert(t);
+    if (b < 0) return 0;
+    push_down(t, b);
+    bool f = (xor_mask >> b) & 1;
+    f ^= !t->child[f];
+    return get_min(t->child[f], xor_mask, b - 1) | (T(f) << b);
+  }
+
+  T get_internal(NodePtr t, int k, int b = kBitWidth - 1) const {
+    if (b < 0) return 0;
+    push_down(t, b);
+    int m = t->child[0] ? t->child[0]->leaf_count : 0;
+    return k < m ? get_internal(t->child[0], k, b - 1)
+                 : get_internal(t->child[1], k - m, b - 1) | (T(1) << b);
+  }
+
+  int count_less(NodePtr t, T val, int b = kBitWidth - 1) const {
+    if (!t || b < 0) return 0;
+    push_down(t, b);
+    bool f = (val >> b) & 1;
+    return (f && t->child[0] ? t->child[0]->leaf_count : 0) +
+           count_less(t->child[f], val, b - 1);
+  }
+};
