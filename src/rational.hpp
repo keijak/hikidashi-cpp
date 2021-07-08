a09d4ffe3d8260e58f2af324a666617013cc0eca
keijak <m@keijak.com>
2021-07-08 11:54:18 +0200

    rational number

--- src/rational.hpp
+++ src/rational.hpp
@@ -1,63 +1,114 @@
 #include <bits/stdc++.h>
 
-template <typename T = long long>
-struct Rational {
- private:
-  using BigInt = __int128_t;
+// #include <boost/multiprecision/cpp_int.hpp>
+// using multip = boost::multiprecision;
+// using multip::abs;
+// using multip::gcd;
 
+template <typename T>
+struct Rational {
  public:
+  using BigInt = __int128_t;
+  // using BigInt = multip::checked_int128_t; // for testing overflow
+
   T nume, deno;
 
   Rational() : nume(0), deno(1) {}
 
-  explicit Rational(T n) : nume(n), deno(1) {}
+  Rational(T n) : nume(n), deno(1) {}
 
-  Rational(T n, T d) : nume(n), deno(d) {
-    assert(deno != 0);
-    if (nume == 0) {
-      deno = 1;
+  Rational(BigInt n, BigInt d) {
+    assert(d != 0);
+    if (n == 0) {
+      d = 1;
     } else {
-      int sign = 1;
-      if ((nume < 0) xor (deno < 0)) {
-        sign = -1;
-      }
-      nume = abs(nume);
-      deno = abs(deno);
-      auto g = std::gcd(nume, deno);
+      const int sign = ((n < 0) xor (d < 0)) ? -1 : 1;
+      if (n < 0) n = -n;
+      if (d < 0) d = -d;
+      const auto g = gcd_(n, d);
       if (g > 1) {
-        nume /= g;
-        deno /= g;
+        n /= g;
+        d /= g;
       }
-      nume *= sign;
+      assert(n <= std::numeric_limits<T>::max());
+      assert(d <= std::numeric_limits<T>::max());
+      n *= sign;
     }
+    nume = static_cast<T>(n);
+    deno = static_cast<T>(d);
   }
 
-  operator double() { return (double)(nume) / deno; }
-  operator long double() { return (long double)(nume) / deno; }
+  Rational(const Rational &x) = default;
+  Rational(Rational &&x) = default;
+  Rational &operator=(const Rational &x) = default;
+  Rational &operator=(Rational &&x) = default;
 
-  friend bool operator<(const Rational &x, const Rational &y) {
-    return (BigInt)(x.nume) * y.deno < (BigInt)(y.nume) * x.deno;
-  }
+  operator double() { return (double)(nume) / (double)(deno); }
+  operator long double() { return (long double)(nume) / (long double)(deno); }
+
+  Rational operator-() const { return {-nume, deno}; }
+  Rational operator+() const { return *this; }
 
   friend bool operator==(const Rational &x, const Rational &y) {
-    return std::tie(x.nume, x.deno) == std::tie(y.nume, y.deno);
+    return (x.nume == y.nume) and (x.deno == y.deno);
+  }
+  friend bool operator!=(const Rational &x, const Rational &y) {
+    return not(x == y);
+  }
+  friend bool operator<(const Rational &x, const Rational &y) {
+    return static_cast<BigInt>(x.nume) * y.deno <
+           static_cast<BigInt>(y.nume) * x.deno;
+  }
+  friend bool operator>(const Rational &x, const Rational &y) { return y < x; }
+  friend bool operator<=(const Rational &x, const Rational &y) {
+    return not(x > y);
+  }
+  friend bool operator>=(const Rational &x, const Rational &y) {
+    return not(x < y);
   }
 
   friend Rational operator+(const Rational &x, const Rational &y) {
-    auto g = std::gcd(x.deno, y.deno);
-    auto zn = x.nume * (y.deno / g) + y.nume * (x.deno / g);
-    auto zd = (x.deno / g) * y.deno;
+    auto g = gcd_(x.deno, y.deno);
+    auto zn = (static_cast<BigInt>(x.nume) * (y.deno / g)) +
+              (static_cast<BigInt>(y.nume) * (x.deno / g));
+    auto zd = static_cast<BigInt>(x.deno / g) * y.deno;
     return Rational(zn, zd);
   }
+  Rational &operator+=(const Rational &x) { return (*this = *this + x); }
+
+  friend Rational operator-(const Rational &x, const Rational &y) {
+    return x + Rational(-y.nume, y.deno);
+  }
+  Rational &operator-=(const Rational &x) { return (*this = *this - x); }
 
   friend Rational operator*(const Rational &x, const Rational &y) {
-    auto g1 = std::gcd(std::abs(x.nume), y.deno);
-    auto g2 = std::gcd(std::abs(y.nume), x.deno);
-    return Rational((x.nume / g1) * (y.nume / g2),
-                    (x.deno / g2) * (y.deno / g1));
+    if (x.nume == 0 or y.nume == 0) return Rational(0);
+    auto g1 = gcd_(abs_(x.nume), y.deno);
+    auto g2 = gcd_(abs_(y.nume), x.deno);
+    return Rational(BigInt(x.nume / g1) * BigInt(y.nume / g2),
+                    BigInt(x.deno / g2) * BigInt(y.deno / g1));
   }
+  Rational &operator*=(const Rational &x) { return (*this = *this * x); }
+
+  Rational inv() const { return Rational(deno, nume); }
+
+  friend Rational operator/(const Rational &x, const Rational &y) {
+    return x * y.inv();
+  }
+  Rational &operator/=(const Rational &x) { return (*this = *this / x); }
 
   friend std::ostream &operator<<(std::ostream &os, const Rational &x) {
     return os << x.nume << "/" << x.deno;
   }
+
+ private:
+  static inline T abs_(T x) { return (x < 0) ? -x : x; }
+  static BigInt gcd_(BigInt a, BigInt b) {
+    if (b == 0) return a;
+    return gcd_(b, a % b);
+  }
 };
+using Rat = Rational<long long>;
+// using Rat = Rational<__int128_t>;
+// using Rat = Rational<multip::checked_int128_t>; // for testing
+// overflow
