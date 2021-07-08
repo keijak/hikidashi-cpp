925b5b7ea433b7a870aaed5a6017b8416eb6e3a8
keijak <m@keijak.com>
2021-07-08 17:52:26 +0200

    bisect

--- src/bisect.hpp
+++ src/bisect.hpp
@@ -7,15 +7,16 @@ using i64 = long long;
 // Returns the boundary argument which satisfies pred(x).
 //
 // Usage:
-//   auto opt_x = bisect(ok, ng, [&](i64 x) -> bool { return ...; });
+//   auto ok_bound = bisect(ok, ng, [&](i64 x) -> bool { return ...; });
 template <class F>
 i64 bisect(i64 true_x, i64 false_x, F pred) {
-  static_assert(std::is_invocable_r_v<bool, F, i64>, "F must be i64->bool");
-  // To allow negative values, use floor_div.
+  static_assert(std::is_invocable_r_v<bool, F, i64>, "F must be: i64 -> bool");
+  // To allow negative values, use floor_div() in the loop.
   assert(true_x >= 0 and false_x >= 0);
+  using u64 = unsigned long long;
 
   while (std::abs(true_x - false_x) > 1) {
-    auto mid = (true_x + false_x) / 2;
+    i64 mid = ((u64)true_x + (u64)false_x) / 2;
     if (pred(mid)) {
       true_x = std::move(mid);
     } else {
