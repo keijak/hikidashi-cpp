c9e58dba687f756fb620936e7d41bf7ff39b8556
keijak <m@keijak.com>
2021-07-12 13:30:53 +0200

    io

--- src/input.hpp
+++ src/input.hpp
@@ -1,6 +1,5 @@
 // Usage:
-//   int n = input;
-//   unsigned x = (unsigned)(input) << 1;
+//   int n = 0; read_unsigned(n);
 
 #pragma GCC target("avx2")
 #pragma GCC optimize("Ofast")
@@ -10,33 +9,31 @@
 #include <cstdio>
 #include <type_traits>
 
-struct Input {
-  inline operator int() { return read_int<int>(); }
-  inline operator long long() { return read_int<long long>(); }
-  inline operator unsigned() { return read_int<unsigned>(); }
-
- private:
-  template <typename T>
-  static T read_int() {
-    T ret = 0, sgn = 1;
-    int ch = getchar_unlocked();
-    while (isspace(ch)) {
-      ch = getchar_unlocked();
-    }
-    if constexpr (!std::is_unsigned<T>::value) {
-      if (ch == '-') {
-        sgn = -1;
-        ch = getchar_unlocked();
-      }
-    }
-    for (; ('0' <= ch) & (ch <= '9'); ch = getchar_unlocked()) {
-      ret = (ret * 10) + (ch - '0');
-    }
-    ungetc(ch, stdin);
-    if constexpr (std::is_unsigned<T>::value) {
-      return ret;
-    } else {
-      return ret * sgn;
-    }
+template <typename T>
+inline void read_unsigned(T& ret) {
+  // ret = 0;  // assumption: ret is already zero-initialized.
+  int ch = getchar_unlocked();
+  for (; isspace(ch); ch = getchar_unlocked())
+    ;
+  for (; isdigit(ch); ch = getchar_unlocked()) {
+    ret = (ret * 10) + (ch - '0');
   }
-} input;
+  // ungetc(ch, stdin);  // assumption: ch is an ignorable whitespace
+}
+
+template <typename T>
+inline void read_signed(T& ret) {
+  // ret = 0;  // assumption: ret is already zero-initialized.
+  int ch = getchar_unlocked(), sign = 1;
+  for (; isspace(ch); ch = getchar_unlocked())
+    ;
+  if (ch == '-') {
+    sign = -1;
+    ch = getchar_unlocked();
+  }
+  for (; isdigit(ch); ch = getchar_unlocked()) {
+    ret = (ret * 10) + (ch - '0');
+  }
+  ret *= sign;
+  // ungetc(ch, stdin);  // assumption: ch is an ignorable whitespace
+}
