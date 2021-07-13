5f08690d84ba4eb10d4af99da4f03445910b7585
keijak <m@keijak.com>
2021-07-13 21:03:59 +0200

    rmq

--- src/range_min_query.hpp
+++ src/range_min_query.hpp
@@ -3,70 +3,72 @@
 template <typename T>
 constexpr int num_bits = CHAR_BIT * sizeof(T);
 
-// Most Significant Set Bit (Highest One Bit)
-inline int mssb_pos(unsigned x) {
+// Log base 2 of the most significant bit which is set.
+inline int msb_log(unsigned x) {
   assert(x != 0);
   return std::numeric_limits<unsigned>::digits - __builtin_clz(x) - 1;
 }
-inline int mssb_pos(unsigned long long x) {
+inline int msb_log(unsigned long long x) {
   assert(x != 0);
   return std::numeric_limits<unsigned long long>::digits - __builtin_clzll(x) -
          1;
 }
 
-template <class T, class BetterOp = std::less<T>, class mask_t = uint32_t>
+// Range Min/Max Query based on Fischer-Heun Structure.
+// - Initialization: O(n)
+// - Query: O(1)
+template <class BetterOp, class mask_t = unsigned>
 struct RMQ {
   static_assert(std::is_integral<mask_t>::value, "mask_t must be integral");
   static_assert(std::is_unsigned<mask_t>::value, "mask_t must be unsigned");
-  static_assert(std::is_invocable_r_v<bool, BetterOp, const T &, const T &>);
+  static_assert(std::is_invocable_r_v<bool, BetterOp, int, int>);
   static constexpr int block_size_ = num_bits<mask_t>;
 
-  int n_;                       // sequence size
-  int block_count_;             // total number of blocks
-  const std::vector<T> &data_;  // sequence data
+  int n_;                 // sequence size
+  int block_count_;       // total number of blocks
+  BetterOp better_than_;  // checks if lhs is strictly better than rhs.
   std::vector<mask_t> indicator_;
   std::vector<std::vector<int>> sparse_table_;
-  BetterOp better_than_;  // checks if lhs is strictly better than rhs.
 
-  explicit RMQ(const std::vector<T> &data, BetterOp better = BetterOp())
-      : n_(data.size()),
+  RMQ(int n, BetterOp better)
+      : n_(n),
         block_count_((n_ + block_size_ - 1) / block_size_),
-        data_(std::move(data)),
+        better_than_(std::move(better)),
         indicator_(n_),
         sparse_table_(
-            block_count_ == 0 ? 0 : mssb_pos(unsigned(block_count_)) + 1,
-            std::vector<int>(block_count_)),
-        better_than_(std::move(better)) {
+            block_count_ == 0 ? 0 : msb_log(unsigned(block_count_)) + 1,
+            std::vector<int>(block_count_)) {
     static constexpr int bufsize = block_size_ + 1;
-    std::array<int, bufsize> buf;            // ring buffer [lp,rp)
-    int lp = 0, rp = 0, rpm1 = bufsize - 1;  // rpm1 = rp-1 (mod bufsize)
+    static int buf[bufsize];       // ring buffer [lp,rp)
+    int lp = 1, rp = 1, rpm1 = 0;  // rpm1 = rp-1 (mod bufsize)
+
+    // Build the indicator table.
     for (int r = 0; r < n_; ++r) {
-      while (lp != rp and (r - buf[lp] >= block_size_)) {
+      while (lp != rp and r - buf[lp] >= block_size_) {
         ++lp;
         if (lp == bufsize) lp = 0;
       }
-      const auto &cur = data_[r];
-      while (lp != rp and not better_than_(data_[buf[rpm1]], cur)) {
+      while (lp != rp and not better_than_(buf[rpm1], r)) {
         rp = rpm1--;
         if (rp == 0) rpm1 = bufsize - 1;
       }
-      mask_t ind = 1;
+      indicator_[r] = 1;
       if (lp != rp) {
-        int p = buf[rpm1];
-        ind |= (indicator_[p] << (r - p));
+        const int p = buf[rpm1];
+        indicator_[r] |= (indicator_[p] << (r - p));
       }
-      indicator_[r] = ind;
       buf[rp] = r;
       rpm1 = rp++;
       if (rp == bufsize) rp = 0;
     }
 
+    // Build the sparse table.
     for (int i = 0; i < block_count_; ++i) {
       sparse_table_[0][i] =
           best_index_small(std::min(block_size_ * (i + 1), n_) - 1);
     }
-    for (int i = 0; i + 1 < int(sparse_table_.size()); ++i) {
-      for (int j = 0; j < block_count_; j++) {
+    for (int i = 0, height = int(sparse_table_.size()) - 1; i < height; ++i) {
+      for (int j = 0; j < block_count_; ++j) {
         sparse_table_[i + 1][j] = better_index(
             sparse_table_[i][j],
             sparse_table_[i][std::min(j + (1 << i), block_count_ - 1)]);
@@ -74,46 +76,53 @@ struct RMQ {
     }
   }
 
-  // Returns the best value in [l, r) (half-open interval).
-  inline const T &query(int l, int r) const {
-    return data_[best_index(l, r - 1)];
+  // Returns the index of the best value in [l, r) (half-open interval).
+  inline int fold(int l, int r) const {
+    assert(l < r);
+    // We internally use closed interval.
+    return best_index(l, r - 1);
   }
 
  private:
   inline int better_index(int i, int j) const {
-    return better_than_(data_[i], data_[j]) ? i : j;
+    return better_than_(i, j) ? i : j;
   }
 
-  // Returns the index of the best value in [r - sz, r] (closed interval).
-  inline int best_index_small(int r, int sz = block_size_) const {
+  // Returns the index of the best value in [r - width, r] (closed interval).
+  inline int best_index_small(int r, int width = block_size_) const {
     assert(r < n_);
-    assert(sz > 0);
-    assert(sz <= block_size_);
+    assert(width > 0);
+    assert(width <= block_size_);
     mask_t ind = indicator_[r];
-    if (sz < block_size_) {
-      ind &= (mask_t(1) << sz) - 1;
+    if (width < block_size_) {
+      ind &= (mask_t(1) << width) - 1;
     }
-    return r - mssb_pos(ind);
+    return r - msb_log(ind);
   }
 
   // Returns the index of the best value in [l, r] (closed interval).
   inline int best_index(int l, int r) const {
     l = std::clamp(l, 0, n_ - 1);
     r = std::clamp(r, 0, n_ - 1);
-    if (r - l + 1 <= block_size_) {
-      return best_index_small(r, r - l + 1);
+    const unsigned width = r - l + 1;
+    if (width <= block_size_) {
+      return best_index_small(r, width);
     }
-    int ql = best_index_small(std::min(l + block_size_, n_) - 1);
-    int qr = best_index_small(r);
+    const int al = best_index_small(std::min(l + block_size_, n_) - 1);
+    const int ar = best_index_small(r);
+    int ans = better_index(al, ar);
     l = l / block_size_ + 1;
     r = r / block_size_ - 1;
-    int ans = better_index(ql, qr);
     if (l <= r) {
-      int i = mssb_pos(unsigned(r - l + 1));
-      int qs =
-          better_index(sparse_table_[i][l], sparse_table_[i][r - (1 << i) + 1]);
-      ans = better_index(ans, qs);
+      const int k = msb_log(width);
+      const int am =
+          better_index(sparse_table_[k][l], sparse_table_[k][r - (1 << k) + 1]);
+      ans = better_index(ans, am);
     }
     return ans;
   }
 };
+template <class BetterOp>
+RMQ<BetterOp> create_rmq(int n, BetterOp better) {
+  return {n, std::move(better)};
+}
