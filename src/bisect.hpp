54eae276256f068bf6c5cd8a14ee62eb2c536607
keijak <m@keijak.com>
2021-08-25 10:12:22 +0200

    bisect_float

--- src/bisect.hpp
+++ src/bisect.hpp
@@ -8,14 +8,29 @@ using i64 = long long;
 //   auto ok_bound = bisect(ok, ng, [&](i64 x) -> bool { return ...; });
 template <class F>
 i64 bisect(i64 true_x, i64 false_x, F pred) {
-  static_assert(std::is_invocable_r_v<bool, F, i64>, "F must be: i64 -> bool");
-  assert(std::max<i64>(true_x, false_x) <= std::numeric_limits<i64>::max() / 2);
+  using u64 = unsigned long long;
+  static_assert(std::is_invocable_r_v<bool, F, i64>);
+
   // To allow negative values, use floor_div() in the loop.
   assert(true_x >= -1 and false_x >= -1);
-  using u64 = unsigned long long;
 
   while (std::abs(true_x - false_x) > 1) {
     i64 mid = ((u64)true_x + (u64)false_x) / 2;
+    if (pred(mid)) {
+      true_x = std::move(mid);
+    } else {
+      false_x = std::move(mid);
+    }
+  }
+  return true_x;
+}
+
+template <class Float, class F>
+Float bisect_float(Float true_x, Float false_x, F pred) {
+  static_assert(std::is_floating_point_v<Float>);
+  static_assert(std::is_invocable_r_v<bool, F, Float>);
+  for (int iter = 0; iter < 80; ++iter) {
+    auto mid = (true_x + false_x) * 0.5;
     if (pred(mid)) {
       true_x = std::move(mid);
     } else {
