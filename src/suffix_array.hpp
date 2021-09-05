25fa122f2e041d183b65664b5bb4bd7023e41fd6
keijak <m@keijak.com>
2021-09-05 07:48:13 +0200

    sa query

--- src/suffix_array.hpp
+++ src/suffix_array.hpp
@@ -3,15 +3,25 @@
 #include <atcoder/string>
 
 // Returns the longest common prefix length of s[i:] and s[j:].
-int lcp_length(const std::string &s, int i, int j) {
+int lcp_length(const std::string &s) {
+  // Initialize
+  assert(not s.empty());
   const int n = s.size();
   const std::vector<int> sa = atcoder::suffix_array(s);
   const std::vector<int> lcp = atcoder::lcp_array(s, sa);
+  assert((int)lcp.size() == n - 1);
   std::vector<int> rsa(n);  // the position of s[i:] in the suffix array.
   for (int i = 0; i < n; ++i) rsa[sa[i]] = i;
-
   SparseTableRMQ rmq(lcp.size(), [&](int i, int j) { return lcp[i] < lcp[j]; });
-  int p = std::min(rsa[i], rsa[j]);
-  int q = std::max(rsa[i], rsa[j]);
-  return lcp[rmq.fold(p, q)];
+
+  // Query
+  auto query_lcp = [&](int i, int j) -> int {
+    if (i == j) {
+      return n - i;
+    }
+    int ri = rsa[i], rj = rsa[j];
+    if (ri > rj) std::swap(ri, rj);
+    assert(ri < rj);
+    return lcp[rmq.fold(ri, rj)];
+  };
 }
