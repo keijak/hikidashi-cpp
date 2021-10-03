8ba0a69406af790e6c526fa80ef3db34dd831eab
keijak <m@keijak.com>
2021-10-03 19:53:00 +0200

    tree diameter

--- src/tree_diameter.hpp
+++ src/tree_diameter.hpp
@@ -1,28 +1,41 @@
-#include <array>
+#include <algorithm>
+#include <cassert>
 #include <vector>
 
 struct Diameter {
   int distance;
-  std::array<int, 2> nodes;
+  std::vector<int> path;
 };
 
 // Tree diameter.
 // Returns the maximum diameter and two endpoint nodes of the diameter.
 Diameter tree_diameter(const std::vector<std::vector<int>> &g) {
+  std::vector<int> parent(g.size());
+
   auto farthest = [&](auto &rec, int v, int p, int d) -> std::pair<int, int> {
-    int res_d = d, res_v = v;
+    parent[v] = p;
+    int rd = d, rv = v;
     for (auto u : g[v]) {
       if (u == p) continue;
       const auto &[child_d, child_v] = rec(rec, u, v, d + 1);
-      if (child_d > res_d) {
-        res_d = child_d;
-        res_v = child_v;
+      if (child_d > rd) {
+        rd = child_d;
+        rv = child_v;
       }
     }
-    return {res_d, res_v};
+    return {rd, rv};
   };
 
-  const auto &[_, u] = farthest(farthest, 0, -1, 0);
-  const auto &[distance, v] = farthest(farthest, u, -1, 0);
-  return Diameter{distance, {u, v}};
+  const auto &[_, vr] = farthest(farthest, 0, -1, 0);
+  const auto &[distance, vl] = farthest(farthest, vr, -1, 0);
+
+  // Restore the path from vl to vr.
+  std::vector<int> diameter_path;
+  for (int v = vl; v != -1; v = parent[v]) {
+    diameter_path.push_back(v);
+  }
+  assert(diameter_path.back() == vr);
+  assert(int(diameter_path.size()) == distance + 1);
+
+  return Diameter{distance, std::move(diameter_path)};
 }
