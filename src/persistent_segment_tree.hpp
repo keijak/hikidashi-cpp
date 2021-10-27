6dd692a7b0fe10a602d386381bbf4dc11581cab8
keijak <m@keijak.com>
2021-10-27 13:46:09 +0200

    binary search in persistent segment tree

--- src/persistent_segment_tree.hpp
+++ src/persistent_segment_tree.hpp
@@ -58,6 +58,7 @@ struct PersistentSegmentTree {
   }
 
   T fold(Int kl, Int kr) const { return fold_(kl, kr, root_, 0, size_); }
+  T fold_all() const { return root_->data; }
   T operator[](Int k) const { return fold_(k, k + 1, root_, 0, size_); }
 
   std::vector<T> to_vec(Int size = -1) const {
@@ -67,6 +68,24 @@ struct PersistentSegmentTree {
       res[i] = (*this)[i];
     }
     return res;
+  }
+
+  template <class F>
+  Int min_left(Int r, F pred) const {
+    static_assert(std::is_invocable_r_v<bool, F, typename Monoid::T, int>,
+                  "predicate must be invocable on (folded, left index).");
+    assert(0 <= r && r <= size_);
+    const auto &fpos = min_left_(r, pred, Monoid::id(), root_, 0, size_).first;
+    return fpos.value_or(-1) + 1;
+  }
+
+  template <class F>
+  Int max_right(Int l, F pred) const {
+    static_assert(std::is_invocable_r_v<bool, F, typename Monoid::T, int>,
+                  "predicate must be invocable on (folded, right index).");
+    assert(0 <= l && l < size_);
+    const auto &fpos = max_right_(l, pred, Monoid::id(), root_, 0, size_).first;
+    return fpos.value_or(size_ + 1) - 1;
   }
 
  private:
@@ -102,19 +121,64 @@ struct PersistentSegmentTree {
     return p;
   }
 
-  NodePtr set_(Int k, T val, NodePtr n, Int l, Int r) const {
-    if (l + 1 == r) return make_leaf(std::move(val));
-    Int m = (l + r) >> 1;
-    if (k < m) return merge(set_(k, std::move(val), n->l, l, m), n->r);
-    return merge(n->l, set_(k, std::move(val), n->r, m, r));
+  NodePtr set_(Int k, T val, NodePtr np, Int nl, Int nr) const {
+    if (nl + 1 == nr) return make_leaf(std::move(val));
+    Int nm = (nl + nr) >> 1;
+    if (k < nm) return merge(set_(k, std::move(val), np->l, nl, nm), np->r);
+    return merge(np->l, set_(k, std::move(val), np->r, nm, nr));
   }
 
-  T fold_(Int kl, Int kr, NodePtr n, Int l, Int r) const {
-    if (n == nil_) return Monoid::id();
-    if (r <= kl or kr <= l) return Monoid::id();
-    if (kl <= l and r <= kr) return n->data;
-    Int m = (l + r) >> 1;
-    return Monoid::op(fold_(kl, kr, n->l, l, m), fold_(kl, kr, n->r, m, r));
+  T fold_(Int kl, Int kr, NodePtr np, Int nl, Int nr) const {
+    if (np == nil_) return Monoid::id();
+    if (nr <= kl or kr <= nl) return Monoid::id();
+    if (kl <= nl and nr <= kr) return np->data;
+    Int nm = (nl + nr) >> 1;
+    return Monoid::op(fold_(kl, kr, np->l, nl, nm),
+                      fold_(kl, kr, np->r, nm, nr));
+  }
+
+  template <class F>
+  std::pair<std::optional<Int>, typename Monoid::T> min_left_(
+      Int kr, F pred, const typename Monoid::T &prval, NodePtr np, Int nl,
+      Int nr) const {
+    if (kr <= nl) return {std::nullopt, Monoid::id()};
+    if (nr <= kr) {
+      auto val = Monoid::op(np->data, prval);
+      if (pred(val, nl)) {
+        return {std::nullopt, std::move(val)};
+      }
+      if (nl + 1 == nr) {
+        return {nl, std::move(val)};
+      }
+    }
+    const Int nm = (nl + nr) >> 1;
+    const auto rsub = min_left_(kr, pred, prval, np->r, nm, nr);
+    if (rsub.first) {
+      return rsub;
+    }
+    return min_left_(kr, pred, std::move(rsub.second), np->l, nl, nm);
+  }
+
+  template <class F>
+  std::pair<std::optional<Int>, typename Monoid::T> max_right_(
+      Int kl, F pred, const typename Monoid::T &plval, NodePtr np, Int nl,
+      Int nr) const {
+    if (nr <= kl) return {std::nullopt, Monoid::id()};
+    if (kl <= nl) {
+      auto val = Monoid::op(plval, np->data);
+      if (pred(val, nl)) {
+        return {std::nullopt, std::move(val)};
+      }
+      if (nl + 1 == nr) {
+        return {nl, std::move(val)};
+      }
+    }
+    const Int nm = (nl + nr) >> 1;
+    const auto lsub = max_right_(kl, pred, plval, np->l, nl, nm);
+    if (lsub.first) {
+      return lsub;
+    }
+    return max_right_(kl, pred, std::move(lsub.second), np->r, nm, nr);
   }
 
   static NodePool *NO_DELETE() {
