f241bf2bd51d5ed0d8d6075cbe8b660cee5863f9
keijak <m@keijak.com>
2021-11-07 22:02:34 +0100

    Make dfs tree O(component_size) by default

--- src/dfs_tree.hpp
+++ src/dfs_tree.hpp
@@ -1,54 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template <bool reshape_graph = false>
 struct DFSTree {
   int n_;
   int root_;
-  vector<vector<int>> g_;  // a spanning tree of the original graph.
-  vector<int> parent_;
-  vector<int> depth_;
+  std::vector<int> parent_;
+  std::vector<int> depth_;
+
+  // if reshape_graph=true, `g_` is mutated into a tree.
+  std::vector<std::vector<int>>& g_;
 
   // edge(u,v) where v is an ancestor of u in the DFS tree.
-  set<pair<int, int>> back_edges_;
+  std::set<std::pair<int, int>> back_edges_;
 
-  explicit DFSTree(vector<vector<int>> g, int root = 0)
-      : n_((int)g.size()), root_(root), g_(g), parent_(n_, -1), depth_(n_, -1) {
+  explicit DFSTree(std::vector<std::vector<int>>& g, int root = 0)
+      : n_(int(g.size())), root_(root), parent_(n_, -1), depth_(n_, -1), g_(g) {
     dfs(root_, -1, 0);
-    for (int v = 0; v < n_; ++v) {
-      std::sort(g[v].begin(), g[v].end());
-    }
   }
 
   bool is_connected(int u, int v) const {
     if (depth_[u] < depth_[v]) std::swap(u, v);
-    return (parent_[u] == v) or back_edges_.count({u, v});
+    return parent_[u] == v or back_edges_.count({u, v});
   }
 
  private:
   void dfs(int v, int p, int d) {
     parent_[v] = p;
     depth_[v] = d;
-    set<int> non_neighbors;
+    std::set<int> non_neighbors;
     for (auto u : g_[v]) {
-      if (u == p) {
-        // parent
-        continue;
-      }
-      if (depth_[u] == -1) {
-        // child
+      if (u == p) continue;   // parent
+      if (depth_[u] == -1) {  // child
         dfs(u, v, d + 1);
         continue;
       }
-      non_neighbors.insert(u);
-      if (depth_[u] < d) {
-        // backward
+      if constexpr (reshape_graph) {
+        non_neighbors.insert(u);
+      }
+      if (depth_[u] < d) {  // backward
         back_edges_.emplace(v, u);
       } else {
         // forward
       }
     }
-    g_[v].erase(std::remove_if(g_[v].begin(), g_[v].end(),
+    if constexpr (reshape_graph) {
+      auto& adj = g_[v];
+      adj.erase(std::remove_if(adj.begin(), adj.end(),
                                [&](int u) { return non_neighbors.count(u); }),
-                g_[v].end());
+                adj.end());
+    }
   }
 };
