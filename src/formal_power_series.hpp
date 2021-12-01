6dfe51a07dfbea8bfc7a508e4d23d3f6c8b7fbcf
keijak <m@keijak.com>
2021-12-01 11:07:44 +0100

    fps default constructor zero

--- src/formal_power_series.hpp
+++ src/formal_power_series.hpp
@@ -369,7 +369,7 @@ struct SparseFPS {
   std::vector<int> degree_;
   std::vector<T> coeff_;
 
-  SparseFPS() : size_(1), degree_(1, 0), coeff_(1, 0) {}  // zero
+  SparseFPS() : size_(0) {}  // zero
 
   explicit SparseFPS(std::vector<std::pair<int, T>> terms)
       : size_(terms.size()), degree_(size_), coeff_(size_) {
