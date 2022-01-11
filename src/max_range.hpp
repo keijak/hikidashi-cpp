df9e85f8349fb399025b4cbab16944016378a37e
keijak <m@keijak.com>
2022-01-11 10:18:00 +0100

    all nearest values problem

--- src/max_range.hpp
+++ src/max_range.hpp
@@ -1,47 +1,11 @@
 // Max/Min Range.
 // Utility for an O(n) solution to Largest Rectangle in Histogram.
-// ref: https://qiita.com/kuuso1/items/318d42cd089a49eeb332
+//
+// Barbay, Fischer & Navarro (2012)
+// ref: https://en.wikipedia.org/wiki/All_nearest_smaller_values
 
-#include <deque>
-#include <limits>
 #include <utility>
 #include <vector>
-
-// For each i, what's the maximum consecutive range in which a[i] is the
-// maximum?
-// Tie-breaking with index: comparing (a[i],i) with (a[j],j).
-// Time complexity: O(n)
-template <typename T>
-std::vector<std::pair<int, int>> max_range(const std::vector<T> &a) {
-  static const T kSentinel = std::numeric_limits<T>::max();
-  const int n = a.size();
-  std::vector<std::pair<int, int>> res(n);
-  std::deque<int> q;
-
-  // left bound
-  for (int i = n - 1; i >= -1; --i) {
-    const auto &x = i >= 0 ? a[i] : kSentinel;
-    while (not q.empty() and a[q.back()] < x) {
-      int j = q.back();
-      res[j].first = i + 1;
-      q.pop_back();
-    }
-    q.push_back(i);
-  }
-  q.clear();
-
-  // right bound
-  for (int i = 0; i <= n; ++i) {
-    const auto &x = i < n ? a[i] : kSentinel;
-    while (not q.empty() and a[q.back()] <= x) {
-      int j = q.back();
-      res[j].second = i;
-      q.pop_back();
-    }
-    q.push_back(i);
-  }
-  return res;
-}
 
 // For each i, what's the maximum consecutive range in which a[i] is the
 // minimum?
@@ -49,32 +13,42 @@ std::vector<std::pair<int, int>> max_range(const std::vector<T> &a) {
 // Time complexity: O(n)
 template <typename T>
 std::vector<std::pair<int, int>> min_range(const std::vector<T> &a) {
-  static const T kSentinel = std::numeric_limits<T>::lowest();
   const int n = a.size();
   std::vector<std::pair<int, int>> res(n);
-  std::deque<int> q;
-
   // left bound
-  for (int i = n - 1; i >= -1; --i) {
-    const auto &x = i >= 0 ? a[i] : kSentinel;
-    while (not q.empty() and a[q.back()] >= x) {
-      int j = q.back();
-      res[j].first = i + 1;
-      q.pop_back();
-    }
-    q.push_back(i);
+  for (int i = 0; i < n; ++i) {
+    int j = i - 1;
+    while (j >= 0 and a[j] > a[i]) j = res[j].first - 1;
+    res[i].first = j + 1;
   }
-  q.clear();
-
   // right bound
-  for (int i = 0; i <= n; ++i) {
-    const auto &x = i < n ? a[i] : kSentinel;
-    while (not q.empty() and a[q.back()] > x) {
-      int j = q.back();
-      res[j].second = i;
-      q.pop_back();
-    }
-    q.push_back(i);
+  for (int i = n - 1; i >= 0; --i) {
+    int j = i + 1;
+    while (j < n and a[j] >= a[i]) j = res[j].second;
+    res[i].second = j;
+  }
+  return res;
+}
+
+// For each i, what's the maximum consecutive range in which a[i] is the
+// maximum?
+// Tie-breaking with index: comparing (a[i],i) with (a[j],j).
+// Time complexity: O(n)
+template <typename T>
+std::vector<std::pair<int, int>> max_range(const std::vector<T> &a) {
+  const int n = a.size();
+  std::vector<std::pair<int, int>> res(n);
+  // left bound
+  for (int i = 0; i < n; ++i) {
+    int j = i - 1;
+    while (j >= 0 and a[j] <= a[i]) j = res[j].first - 1;
+    res[i].first = j + 1;
+  }
+  // right bound
+  for (int i = n - 1; i >= 0; --i) {
+    int j = i + 1;
+    while (j < n and a[j] < a[i]) j = res[j].second;
+    res[i].second = j;
   }
   return res;
 }
