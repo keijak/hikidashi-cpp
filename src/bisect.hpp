3c437c6124926c6c784c5d04b27b9b60e363d3e3
keijak <m@keijak.com>
2022-01-24 10:54:37 +0100

    bisect

--- src/bisect.hpp
+++ src/bisect.hpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 // Safe (a+b)/2 on integers
-template<class T>
+template <class T>
 T midpoint(T a, T b) {
   using U = std::make_unsigned_t<T>;
   int sign = 1;
@@ -11,33 +11,27 @@ T midpoint(T a, T b) {
 }
 
 // Binary search over integers
-template<class T, class F>
+template <class T, class F>
 auto bisect(T truthy, T falsy, F pred) -> T {
   static_assert(std::is_integral_v<T>);
   static_assert(std::is_invocable_r_v<bool, F, T>);
   while (std::max(truthy, falsy) - std::min(truthy, falsy) > 1) {
-    T mid = midpoint(truthy, falsy);
-    if (pred(mid)) {
-      truthy = std::move(mid);
-    } else {
-      falsy = std::move(mid);
-    }
+    auto mid = midpoint(truthy, falsy);
+    auto ok = pred(mid);
+    (ok ? truthy : falsy) = std::move(mid);
   }
   return truthy;
 }
 
 // Binary search over floating numbers
-template<class Float, class F>
+template <class Float, class F>
 Float bisect_float(Float truthy, Float falsy, F pred) {
   static_assert(std::is_floating_point_v<Float>);
   static_assert(std::is_invocable_r_v<bool, F, Float>);
   for (int iter = 0; iter < 80; ++iter) {
     auto mid = (truthy + falsy) * 0.5;
-    if (pred(mid)) {
-      truthy = std::move(mid);
-    } else {
-      falsy = std::move(mid);
-    }
+    auto ok = pred(mid);
+    (ok ? truthy : falsy) = std::move(mid);
   }
   return truthy;
 }
