e1c8594b76e12a2dbcd3fd3df886f3e250148d92
keijak <m@keijak.com>
2022-02-07 00:25:08 +0100

    binary trie: split & merge

--- src/binary_trie.hpp
+++ src/binary_trie.hpp
@@ -14,39 +14,27 @@ struct BinaryTrie {
     mutable T lazy_mask;
     Node() : leaf_count(0), child{nullptr, nullptr}, lazy_mask(0) {}
   };
-  NodePtr root_;  // The root node.
+  NodePtr root_;
 
-  struct NodePool {
-    static constexpr size_t kInitialBlockSize = 1u << 12;
-    static constexpr double kBlockSizeGrowthRate = 1.5;  // Try smaller rate if MLE.
+  BinaryTrie() = default;
+  explicit BinaryTrie(NodePtr p) : root_(p) {}
 
-    std::vector<std::unique_ptr<Node[]>> blocks_;
-    size_t bsize_;
-    size_t bi_;
-    size_t ni_;
-
-    NodePool() : bsize_(kInitialBlockSize), bi_(0), ni_(0) {
-      blocks_.emplace_back(new Node[kInitialBlockSize]);
-    }
-
-    NodePtr new_node() {
-      if (ni_ == bsize_) {
-        bi_++;
-        ni_ = 0;
-        bsize_ *= kBlockSizeGrowthRate;
-        blocks_.emplace_back(new Node[bsize_]);
-      }
-      return &blocks_[bi_][ni_++];
-    }
-  };
-  NodePool *pool_;
-
-  BinaryTrie() : root_(nullptr), pool_(NO_DELETE()) {}
-  BinaryTrie(NodePool *pool) : root_(nullptr), pool_(pool) {}
-
-  int size() const { return root_ ? root_->leaf_count : 0; }
+  int size() const { return size(root_); }
 
   bool empty() const { return size() == 0; }
+
+  // Applies an XOR-mask to all elements.
+  void xor_all(T xor_mask) {
+    if (root_) root_->lazy_mask ^= xor_mask;
+  }
+
+  // Converts the multiset to a vector.
+  std::vector<T> to_vec() const {
+    std::vector<T> res;
+    res.reserve(size());
+    to_vec_internal(root_, T(0), res);
+    return res;
+  }
 
   void insert(T val) { root_ = insert_internal(root_, val); }
 
@@ -90,19 +78,24 @@ struct BinaryTrie {
     return t->leaf_count;
   }
 
-  void xor_all(T xor_mask) {
-    if (root_) root_->lazy_mask ^= xor_mask;
+  // Splits the trie into two: k smallest items and (n-k) largest items.
+  std::pair<BinaryTrie, BinaryTrie> split(int k) {
+    auto [t1, t2] = split_internal(root_, k);
+    return {BinaryTrie(t1), BinaryTrie(t2)};
   }
 
-  std::vector<T> to_vec() const {
-    std::vector<T> res;
-    res.reserve(size());
-    to_vec_internal(root_, T(0), res);
-    return res;
-  }
+  // Merges two tries into this. Destructs the other.
+  void merge(BinaryTrie other) { root_ = merge_internal(root_, other.root_); }
 
  private:
-  void push_down(NodePtr t, int b) const {
+  static NodePtr make_node(int leaf_count, NodePtr l = nullptr,
+                           NodePtr r = nullptr) {
+    static std::deque<Node> pool;
+    pool.emplace_back(leaf_count, l, r);
+    return &pool.back();
+  }
+
+  static void push_down(NodePtr t, int b) {
     if (t->lazy_mask == 0) return;
     if ((t->lazy_mask >> b) & 1) std::swap(t->child[0], t->child[1]);
     if (t->child[0]) t->child[0]->lazy_mask ^= t->lazy_mask;
@@ -110,8 +103,10 @@ struct BinaryTrie {
     t->lazy_mask = 0;
   }
 
-  NodePtr insert_internal(NodePtr t, T val, int b = kBitWidth - 1) {
-    if (not t) t = pool_->new_node();
+  static int size(NodePtr t) { return (t == nullptr) ? 0 : t->leaf_count; }
+
+  static NodePtr insert_internal(NodePtr t, T val, int b = kBitWidth - 1) {
+    if (not t) t = make_node(0);
     t->leaf_count += 1;
     if (b < 0) return t;
     push_down(t, b);
@@ -120,7 +115,7 @@ struct BinaryTrie {
     return t;
   }
 
-  NodePtr erase_internal(NodePtr t, T val, int b = kBitWidth - 1) {
+  static NodePtr erase_internal(NodePtr t, T val, int b = kBitWidth - 1) {
     assert(t);
     t->leaf_count -= 1;
     if (t->leaf_count == 0) return nullptr;
@@ -131,7 +126,7 @@ struct BinaryTrie {
     return t;
   }
 
-  T get_min(NodePtr t, T xor_mask, int b = kBitWidth - 1) const {
+  static T get_min(NodePtr t, T xor_mask, int b = kBitWidth - 1) {
     assert(t);
     if (b < 0) return 0;
     push_down(t, b);
@@ -140,29 +135,29 @@ struct BinaryTrie {
     return get_min(t->child[f], xor_mask, b - 1) | (T(f) << b);
   }
 
-  T get_internal(NodePtr t, int k, int b = kBitWidth - 1) const {
+  static T get_internal(NodePtr t, int k, int b = kBitWidth - 1) {
     if (b < 0) return 0;
     push_down(t, b);
-    int m = t->child[0] ? t->child[0]->leaf_count : 0;
+    int m = size(t->child[0]);
     return k < m ? get_internal(t->child[0], k, b - 1)
                  : get_internal(t->child[1], k - m, b - 1) | (T(1) << b);
   }
 
-  int count_less(NodePtr t, T val, int b = kBitWidth - 1) const {
+  static int count_less(NodePtr t, T val, int b = kBitWidth - 1) {
     if (not t or b < 0) return 0;
     push_down(t, b);
     bool f = (val >> b) & 1;
-    return (f and t->child[0] ? t->child[0]->leaf_count : 0) +
-           count_less(t->child[f], val, b - 1);
+    return (f ? size(t->child[0]) : 0) + count_less(t->child[f], val, b - 1);
   }
 
-  void to_vec_internal(NodePtr t, T val, std::vector<T> &out,
-                       int b = kBitWidth - 1) const {
+  static void to_vec_internal(NodePtr t, T val, std::vector<T> &out,
+                              int b = kBitWidth - 1) {
     if (not t) return;
     if (b < 0) {
       out.push_back(val);
       return;
     }
+    push_down(t, b);
     if (t->child[0]) {
       to_vec_internal(t->child[0], val, out, b - 1);
     }
@@ -171,9 +166,37 @@ struct BinaryTrie {
     }
   }
 
-  static NodePool *NO_DELETE() {
-    static NodePool kNoDeletePool;
-    return &kNoDeletePool;
+  static std::pair<NodePtr, NodePtr> split_internal(NodePtr t, int pos,
+                                                    int b = kBitWidth - 1) {
+    if (not t) return {nullptr, nullptr};
+    if (b < 0) {
+      if (pos <= 0)
+        return {nullptr, t};
+      else
+        return {t, nullptr};
+    }
+    push_down(t, b);
+    const int lsize = size(t->child[0]);
+    if (lsize <= pos) {
+      auto [lz, rz] = split_internal(t->child[1], pos - lsize, b - 1);
+      return {make_node(size(t->child[0]) + size(lz), t->child[0], lz),
+              make_node(size(rz), nullptr, rz)};
+    } else {
+      auto [lz, rz] = split_internal(t->child[0], pos, b - 1);
+      return {make_node(size(lz), lz, nullptr),
+              make_node(size(rz) + size(t->child[1]), rz, t->child[1])};
+    }
+  }
+
+  static NodePtr merge_internal(NodePtr t1, NodePtr t2, int b = kBitWidth - 1) {
+    if (not t2) return t1;
+    if (not t1) return t2;
+    push_down(t1, b);
+    push_down(t2, b);
+    t1->leaf_count = size(t1) + size(t2);
+    t1->child[0] = merge_internal(t1->child[0], t2->child[0], b - 1);
+    t1->child[1] = merge_internal(t1->child[1], t2->child[1], b - 1);
+    return t1;
   }
 };
 using Trie = BinaryTrie<>;
