5bb411f056af6a10503ca3d45ebc51e564a41bb7
keijak <m@keijak.com>
2022-03-22 00:38:31 +0100

    centroid

--- src/centroid_decomposition.hpp
+++ src/centroid_decomposition.hpp
@@ -1,25 +1,37 @@
 #include <bits/stdc++.h>
 
 struct CentroidTree {
-  int n_;
-  std::vector<int> cpar_;  // parent of each node in the centroid tree
+  int n;                    // number of nodes.
+  int root;                 // root of the centroid tree.
+  std::vector<int> parent;  // parent of each node in the centroid tree.
+  std::vector<std::vector<int>> graph;  // adjacent nodes in the centroid tree.
 
-  explicit CentroidTree(std::vector<std::vector<int>> g, int root = 0)
-      : n_(g.size()), cpar_(n_, -1) {
-    std::vector<std::set<int>> gd(n_);
-    std::vector<int> subsize(n_, 0);
-    for (int v = 0; v < n_; ++v) {
+  explicit CentroidTree(std::vector<std::vector<int>> g)
+      : n(g.size()), root(-1), parent(n, -1), graph(n) {
+    std::vector<std::set<int>> gd(n);
+    std::vector<int> subsize(n, 0);
+    for (int v = 0; v < n; ++v) {
       gd[v].insert(g[v].begin(), g[v].end());
     }
-    build(root, -1, gd, subsize);
+    const int start_node = 0;
+    build(start_node, -1, gd, subsize);
+    for (int v = 0; v < n; ++v) {
+      const int p = parent[v];
+      if (p == -1) {
+        root = v;
+      } else {
+        graph[p].push_back(v);
+        graph[v].push_back(p);
+      }
+    }
   }
 
  private:
   void build(int v, int p, std::vector<std::set<int>> &gd,
              std::vector<int> &subsize) {
-    const int n = dfs_subsize(v, p, gd, subsize);  // fill subsize
-    const int c = centroid(v, p, n, gd, subsize);  // find centroid
-    cpar_[c] = p;
+    const int sz = dfs_subsize(v, p, gd, subsize);  // fill subsize
+    const int c = centroid(v, p, sz, gd, subsize);  // find centroid
+    parent[c] = p;
 
     std::vector<int> tmp(gd[c].begin(), gd[c].end());
     for (auto u : tmp) {
@@ -39,12 +51,12 @@ struct CentroidTree {
     return subsize[v];
   }
 
-  int centroid(int v, int p, int n, const std::vector<std::set<int>> &gd,
+  int centroid(int v, int p, int sz, const std::vector<std::set<int>> &gd,
                const std::vector<int> &subsize) {
     for (auto u : gd[v]) {
       if (u == p) continue;
-      if (subsize[u] > n / 2) {
-        return centroid(u, v, n, gd, subsize);
+      if (subsize[u] > sz / 2) {
+        return centroid(u, v, sz, gd, subsize);
       }
     }
     return v;
