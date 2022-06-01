fbe21a0fa9f7abcbe2280ac6ca4291ef8ec23054
keijak <m@keijak.com>
2022-06-01 21:00:13 +0200

    add sparse set

---
+++ src/sparse_set.hpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+struct SparseSet {
+  static constexpr int kMaxSize = int(1e5) + 10;
+  static constexpr int kMaxValue = int(1e5) + 10;
+
+  int size_ = 0;
+  std::array<int, kMaxSize> dense_;
+  std::array<int, kMaxValue> sparse_;
+
+  // O(1)
+  bool contains(int x) const {
+    // May access uninitialized memory, but safe.
+    const size_t i = sparse_[x];
+    return i < (size_t)size_ and dense_[i] == x;
+  }
+
+  // O(1)
+  void insert(int x) {
+    if (contains(x)) return;
+    dense_[size_] = x;
+    sparse_[x] = size_++;
+  }
+
+  // O(1)
+  void erase(int x) {
+    const int i = sparse_[x];
+    if (i < 0 or i >= size_ or dense_[i] != x) return;
+    const auto y = dense_[--size_];
+    dense_[i] = y;
+    sparse_[y] = i;
+  }
+
+  // O(1)
+  inline void clear() { size_ = 0; }
+  inline int size() const { return size_; }
+  inline bool empty() const { return size_ == 0; }
+
+  // O(size)
+  template <class F>
+  void for_each(F func) {
+    for (int i = 0; i < size_; ++i) {
+      func(dense_[i]);
+    }
+  }
+
+  // For silencing warnings on accessing uninitialized memory.
+  void safe_init() {
+    dense_.fill(0);
+    sparse_.fill(0);
+  }
+};
