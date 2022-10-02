b392edd1778a8022b56d23e552a18c3ea2e3b39b
keijak <m@keijak.com>
2022-10-02 19:52:06 +0200

    noshi xor basis

--- src/xor_basis.hpp
+++ src/xor_basis.hpp
@@ -1,24 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// basis[j] = {y, i} means:
-// - j-th bit を a[i] が担当する
-// - y の highest 1-bit が (1 << j)
-// - a[0] 〜 a[i] を使って y が作れる
+using Uint = unsigned long long;
 
-using BitSet = unsigned long long;
+// Noshi Basis
+//
+// basis[j] = {x, i} means:
+// - x の highest bit を a[i] が担当する
+// - a[0] 〜 a[i] を使って x が作れる
+//
+// ref. https://qiita.com/Sooh31/items/97e2d9724fe47ed238b3
 
-std::vector<std::optional<std::pair<BitSet, int>>> xor_basis(
-    const std::vector<BitSet> &a, int m) {
-  const int n = (int)a.size();
-  std::vector<std::optional<std::pair<BitSet, int>>> basis(m);
-  for (int i = 0; i < n; ++i) {
+std::vector<std::pair<Uint, int>> xor_basis(const std::vector<Uint>& a) {
+  std::vector<std::pair<Uint, int>> basis;
+  for (int i = 0; i < (int)a.size(); ++i) {
+    auto x = a[i];
+    for (const auto& [xk, k] : basis) {
+      x = std::min(x, x ^ xk);
+    }
+    if (x != 0) {
+      basis.emplace_back(x, i);
+    }
+  }
+  return basis;
+}
+
+// basis[j] = {x, i} means:
+// - x の highest bit が j であり、a[i] が担当する
+// - a[0] 〜 a[i] を使って x が作れる
+
+std::vector<std::optional<std::pair<Uint, int>>> xor_basis(
+    const std::vector<Uint>& a, int m) {
+  std::vector<std::optional<std::pair<Uint, int>>> basis(m);
+  for (int i = 0; i < (int)a.size(); ++i) {
     auto x = a[i];
     for (int j = m - 1; j >= 0; --j) {
       if (not(x >> j & 1)) continue;
       if (basis[j].has_value()) {
-        auto [y, k] = basis[j].value();
-        x ^= y;
+        auto [xk, k] = basis[j].value();
+        x ^= xk;
       } else {
         basis[j] = pair(x, i);
         break;
