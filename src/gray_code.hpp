7dcc4a8b0391fa2604ebe26b08e142a746522e9f
keijak <m@keijak.com>
2022-10-02 22:16:17 +0200

    graycode

--- src/gray_code.hpp
+++ src/gray_code.hpp
@@ -1,8 +1,35 @@
+// https://en.wikipedia.org/wiki/Gray_code
 
-int gray_code(int n) { return n ^ (n >> 1); }
+unsigned to_gray_code(unsigned n) { return n ^ (n >> 1); }
 
-int inv_gray_code(int g) {
-  int n = 0;
-  for (; g; g >>= 1) n ^= g;
-  return n;
+unsigned from_gray_code(unsigned num) {
+  auto mask = num;
+  while (mask) {
+    mask >>= 1;
+    num ^= mask;
+  }
+  return num;
+}
+
+// A more efficient version for Gray codes 32 bits or fewer through the use of
+// SWAR (SIMD within a register) techniques. It implements a parallel prefix
+// XOR function. The assignment statements can be in any order.
+
+uint32_t from_gray_code32(uint32_t num) {
+  num ^= num >> 16;
+  num ^= num >> 8;
+  num ^= num >> 4;
+  num ^= num >> 2;
+  num ^= num >> 1;
+  return num;
+}
+
+uint64_t from_gray_code64(uint64_t num) {
+  num ^= num >> 32;
+  num ^= num >> 16;
+  num ^= num >> 8;
+  num ^= num >> 4;
+  num ^= num >> 2;
+  num ^= num >> 1;
+  return num;
 }
