70308a5806e7ae339930d33cfee2a10d06d2730a
keijak <m@keijak.com>
2022-10-02 22:05:27 +0200

    kadane

--- src/max_subarray.hpp
+++ src/max_subarray.hpp
@@ -1,11 +1,10 @@
 // Kadane's Algorithm
 // Returns max(sum(s[l:r])).
-// - `a` must not be empty (or provide a default value).
 // - `a[i]` can be negative.
+// - Selecting an empty range is allowed (returns 0).
 template <typename T>
 T max_subarray(const vector<T>& a) {
-  assert(!a.empty());
-  T res = a[0];  // default value
+  T res = 0;
   T sum = 0;
   for (const auto& x : a) {
     sum = max(sum + x, x);
