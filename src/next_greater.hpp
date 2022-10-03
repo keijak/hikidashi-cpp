dccb8af5a67866e534a231fdcedc66ffef22ee0f
keijak <m@keijak.com>
2022-10-03 09:57:19 +0200

    next_greater

--- src/next_greater.hpp
+++ src/next_greater.hpp
@@ -8,34 +8,11 @@
 #include <vector>
 
 // For each i, what's the maximum consecutive range in which a[i] is the
-// minimum?
-// Tie-breaking with index: comparing (a[i],i) with (a[j],j).
-// Time complexity: O(n)
-template <typename T>
-std::vector<std::pair<int, int>> min_range(const std::vector<T> &a) {
-  const int n = a.size();
-  std::vector<std::pair<int, int>> res(n);
-  // left bound
-  for (int i = 0; i < n; ++i) {
-    int j = i - 1;
-    while (j >= 0 and a[j] > a[i]) j = res[j].first - 1;
-    res[i].first = j + 1;
-  }
-  // right bound
-  for (int i = n - 1; i >= 0; --i) {
-    int j = i + 1;
-    while (j < n and a[j] >= a[i]) j = res[j].second;
-    res[i].second = j;
-  }
-  return res;
-}
-
-// For each i, what's the maximum consecutive range in which a[i] is the
 // maximum?
 // Tie-breaking with index: comparing (a[i],i) with (a[j],j).
 // Time complexity: O(n)
 template <typename T>
-std::vector<std::pair<int, int>> max_range(const std::vector<T> &a) {
+std::vector<std::pair<int, int>> next_greater(const std::vector<T> &a) {
   const int n = a.size();
   std::vector<std::pair<int, int>> res(n);
   // left bound
@@ -48,6 +25,29 @@ std::vector<std::pair<int, int>> max_range(const std::vector<T> &a) {
   for (int i = n - 1; i >= 0; --i) {
     int j = i + 1;
     while (j < n and a[j] < a[i]) j = res[j].second;
+    res[i].second = j;
+  }
+  return res;
+}
+
+// For each i, what's the maximum consecutive range in which a[i] is the
+// minimum?
+// Tie-breaking with index: comparing (a[i],i) with (a[j],j).
+// Time complexity: O(n)
+template <typename T>
+std::vector<std::pair<int, int>> next_smaller(const std::vector<T> &a) {
+  const int n = a.size();
+  std::vector<std::pair<int, int>> res(n);
+  // left bound
+  for (int i = 0; i < n; ++i) {
+    int j = i - 1;
+    while (j >= 0 and a[j] > a[i]) j = res[j].first - 1;
+    res[i].first = j + 1;
+  }
+  // right bound
+  for (int i = n - 1; i >= 0; --i) {
+    int j = i + 1;
+    while (j < n and a[j] >= a[i]) j = res[j].second;
     res[i].second = j;
   }
   return res;
