d9cb6b1bea4781a2c67e644c06521f961c2d6fd7
keijak <m@keijak.com>
2022-11-15 23:36:38 +0100

    no inline

--- src/bitops.hpp
+++ src/bitops.hpp
@@ -28,61 +28,61 @@ int popcount(T x) {
   assert(false);  // unsupported type
 }
 
-inline bool has_single_bit(unsigned x) { return __builtin_popcount(x) == 1; }
-inline int countl_zero(unsigned x) {
+bool has_single_bit(unsigned x) { return __builtin_popcount(x) == 1; }
+int countl_zero(unsigned x) {
   if (x == 0) return std::numeric_limits<unsigned>::digits;
   return __builtin_clz(x);
 }
-inline int countr_zero(unsigned x) {
+int countr_zero(unsigned x) {
   if (x == 0) return std::numeric_limits<unsigned>::digits;
   return __builtin_ctz(x);
 }
-inline int countl_one(unsigned x) {
+int countl_one(unsigned x) {
   x = ~x;
   if (x == 0) return std::numeric_limits<unsigned>::digits;
   return __builtin_clz(x);
 }
-inline int countr_one(unsigned x) {
+int countr_one(unsigned x) {
   x = ~x;
   if (x == 0) return std::numeric_limits<unsigned>::digits;
   return __builtin_ctz(x);
 }
 
-inline bool has_single_bit(Uint x) { return __builtin_popcountll(x) == 1; }
-inline int countl_zero(Uint x) {
+bool has_single_bit(Uint x) { return __builtin_popcountll(x) == 1; }
+int countl_zero(Uint x) {
   if (x == 0) return std::numeric_limits<Uint>::digits;
   return __builtin_clzll(x);
 }
-inline int countr_zero(Uint x) {
+int countr_zero(Uint x) {
   if (x == 0) return std::numeric_limits<Uint>::digits;
   return __builtin_ctzll(x);
 }
-inline int countl_one(Uint x) {
+int countl_one(Uint x) {
   x = ~x;
   if (x == 0) return std::numeric_limits<Uint>::digits;
   return __builtin_clzll(x);
 }
-inline int countr_one(Uint x) {
+int countr_one(Uint x) {
   x = ~x;
   if (x == 0) return std::numeric_limits<Uint>::digits;
   return __builtin_ctzll(x);
 }
 
-inline int bit_width(unsigned x) {
+int bit_width(unsigned x) {
   if (x == 0) return 0;
   return std::numeric_limits<unsigned>::digits - __builtin_clz(x);
 }
-inline int bit_width(Uint x) {
+int bit_width(Uint x) {
   if (x == 0) return 0;
   return std::numeric_limits<Uint>::digits - __builtin_clzll(x);
 }
 template <typename T, typename U = std::make_unsigned_t<T>>
-inline U bit_floor(T x) {
+U bit_floor(T x) {
   if (x <= 2) return x;
   return U(1) << (bit_width(U(x)) - 1);
 }
 template <typename T, typename U = std::make_unsigned_t<T>>
-inline U bit_ceil(T x) {
+U bit_ceil(T x) {
   if (x <= 2) return x;
   return bit_floor(U(x - 1)) << 1;
 }
@@ -90,41 +90,41 @@ inline U bit_ceil(T x) {
 // Most significant bit that is set.
 // - msb(x) = bit_floor(x)
 // - msb_log(x) = bit_width(x) - 1
-inline int msb_log(unsigned x) {
+int msb_log(unsigned x) {
   assert(x != 0);
   return std::numeric_limits<unsigned>::digits - __builtin_clz(x) - 1;
 }
-inline int msb_log(Uint x) {
+int msb_log(Uint x) {
   assert(x != 0);
   return std::numeric_limits<Uint>::digits - __builtin_clzll(x) - 1;
 }
 template <typename T, typename U = std::make_unsigned_t<T>>
-inline U msb(T x) {
+U msb(T x) {
   if (x == 0) return 0;
   return U(1) << msb_log(U(x));
 }
 template <typename T, typename U = std::make_unsigned_t<T>>
-inline U drop_msb(T x) {
+U drop_msb(T x) {
   return U(x) ^ msb(U(x));
 }
 
 // Least significant bit that is set.
 // - lsb(x) = (x & -x)
 // - lsb_log(x) = countr_zero(x)
-inline int lsb_log(unsigned x) {
+int lsb_log(unsigned x) {
   assert(x != 0);
   return __builtin_ctz(x);
 }
-inline int lsb_log(Uint x) {
+int lsb_log(Uint x) {
   assert(x != 0);
   return __builtin_ctzll(x);
 }
 template <typename T, typename U = std::make_unsigned_t<T>>
-inline U lsb(T x) {
+U lsb(T x) {
   return (x & -x);
 }
 template <typename T, typename U = std::make_unsigned_t<T>>
-inline U drop_lsb(T x) {
+U drop_lsb(T x) {
   return x & (x - 1);
 }
 
